e3568b6b8cf1e879ba946a3d0a4176ec1dd4877d
Yoshi2112 <joshua.s.williams@uon.edu.au>
2018-10-29 16:38:19 +1100

    again?

--- python/particle motion.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul 10 15:19:57 2017
-
-@author: c3134027
-"""
-
-import numpy as np
-import matplotlib.pyplot as plt
--- python/test3.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Sep 22 11:48:11 2017
-
-@author: iarey
-"""
-
-A = 0
