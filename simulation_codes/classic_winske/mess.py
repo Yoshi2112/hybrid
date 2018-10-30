d04c257f8cd466686d4b4e95c58f099ff06f774e
Yoshi2112 <joshua.s.williams@uon.edu.au>
2018-10-30 13:11:39 +1100

    python and fortran hybrids

--- simulation_codes/classic_winske/mess.py
+++ simulation_codes/classic_winske/mess.py
@@ -1,19 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pdb
 
 
-def trans():
-    den = np.zeros(nx + 2)                    #  zero source arrays
+def trans(by, bz, ex, ey, ez):
+    #  Zero source arrays
+    den = np.zeros(nx + 2)                    
     vix = np.zeros(nx + 2)
     viy = np.zeros(nx + 2)
     viz = np.zeros(nx + 2)
 
-    dns = np.zeros((nx + 2, nsp))
-    vxs = np.zeros((nx + 2, nsp))
-    vys = np.zeros((nx + 2, nsp))
-    vzs = np.zeros((nx + 2, nsp))
-
-    parmov()                                        # move particles and do particle diagnostics
+    dns, vxs, vys, vzs = parmov(by, bz, ex, ey, ez)     # move particles and do particle diagnostics
 
     for k in range(nsp):                            # put ghost cell contributions into real domain
         dns[1, k]   += dns[nx + 1, k]
@@ -62,10 +59,16 @@ def trans():
 #      call smooth(vix,nx2)
 #      call smooth(viy,nx2)
 #      call smooth(viz,nx2)
-    return
+    return den, vix, viy, viz
 
 
-def parmov():
+def parmov(by, bz, ex, ey, ez):
+    # Zero particle moments
+    dns = np.zeros((nx + 2, nsp))
+    vxs = np.zeros((nx + 2, nsp))
+    vys = np.zeros((nx + 2, nsp))
+    vzs = np.zeros((nx + 2, nsp))
+    
     acc1 = 0; acc2 = 0
     for kk in range(nsp):
         acc2  += nspec[kk]
@@ -73,9 +76,21 @@ def parmov():
         dta  = dt / wspec[isp]
         dta2 = .5 * dta
 
-        for l in range(acc1, acc2):
-            i     = int(x[l] / hx + 0.5)
-            fx    = x[l] / hx + 0.5 - i
+# =============================================================================
+#         # REMOVE LATER (HERE TO STOP FIELDS BLOWING UP PARTICLES)
+#         foy = np.zeros(nx + 2)             # Force?
+#         foz = np.zeros(nx + 2)             # Force?
+#         by  = np.ones(nx + 2) * byc        # Magnetic field: y
+#         bz  = np.ones(nx + 2) * bzc        # Magnetic field: z
+#         ex  = np.zeros(nx + 2)             # Electric field: x
+#         ey  = np.zeros(nx + 2)             # Electric field: y
+#         ez  = np.zeros(nx + 2)             # Electric field: z
+#         ##################
+# =============================================================================
+        
+        for ii in range(acc1, acc2):
+            i     = int(x[ii] / hx + 0.5)
+            fx    = x[ii] / hx + 0.5 - i
             fxc   = 1. - fx
 
             exa   = ex[i] * fxc + ex[i + 1] * fx
@@ -85,29 +100,32 @@ def parmov():
             bza   = bz[i] * fxc + bz[i + 1] * fx
 
             f     = 1. - dta * dta2 * (bxc**2 + bya**2 + bza**2)
-            g     = dta2 * (vx[l]*bxc+vy[l]*bya+vz[l]*bza)
+            g     = dta2 * (vx[ii]*bxc + vy[ii]*bya + vz[ii]*bza)
 
-            vxa   = vx[l] + dta2 * exa
-            vya   = vy[l] + dta2 * eya
-            vza   = vz[l] + dta2 * eza
+            vxa   = vx[ii] + dta2 * exa
+            vya   = vy[ii] + dta2 * eya
+            vza   = vz[ii] + dta2 * eza
 
-            vx[l] = f * vx[l] + dta * (exa + vya*bza - vza*bya + g*bxc)
-            vy[l] = f * vy[l] + dta * (eya + vza*bxc - vxa*bza + g*bya)
-            vz[l] = f * vz[l] + dta * (eza + vxa*bya - vya*bxc + g*bza)
+            vx[ii] = f * vx[ii] + dta * (exa + vya*bza - vza*bya + g*bxc)
+            vy[ii] = f * vy[ii] + dta * (eya + vza*bxc - vxa*bza + g*bya)
+            vz[ii] = f * vz[ii] + dta * (eza + vxa*bya - vya*bxc + g*bza)
 
-            dx    = vx[l] * dt
-            x[l]  = x[l]  + dx
+            dx    = vx[ii] * dt
+            x[ii]  = x[ii]  + dx
 
         # check to see particle is still in simulation region
-            if x[l] >= xmax:
-                x[l] -= xmax
-            if x[l] < 0:
-                x[l] += xmax
+            if x[ii] >= xmax:
+                x[ii] -= xmax
+            if x[ii] < 0:
+                x[ii] += xmax
 
         # push extra half time step to collect density and current
-            i     = int(x[l] / hx + 0.5)
-            fx    = x[l] / hx + 0.5 - i
-            fxc   = 1. - fx
+            try:
+                i     = int(x[ii] / hx + 0.5)
+                fx    = x[ii] / hx + 0.5 - i
+                fxc   = 1. - fx
+            except:
+                pdb.set_trace()
 
             exa = ex[i]*fxc + ex[i + 1]*fx
             eya = ey[i]*fxc + ey[i + 1]*fx + foy[i]*fxc + foy[i + 1]*fx
@@ -116,10 +134,10 @@ def parmov():
             bya = by[i]*fxc + by[i + 1]*fx
             bza = bz[i]*fxc + bz[i + 1]*fx
 
-            vxa = vx[l] + dta2 * (exa + vy[l]*bza - vz[l]*bya)
-            vya = vy[l] + dta2 * (eya + vz[l]*bxc - vx[l]*bza)
-            vza = vz[l] + dta2 * (eza + vx[l]*bya - vy[l]*bxc)
-
+            vxa = vx[ii] + dta2 * (exa + vy[ii]*bza - vz[ii]*bya)
+            vya = vy[ii] + dta2 * (eya + vz[ii]*bxc - vx[ii]*bza)
+            vza = vz[ii] + dta2 * (eza + vx[ii]*bya - vy[ii]*bxc)
+            
             vxs[i, isp]    += vxa*fxc
             vxs[i + 1,isp] += vxa*fx
             vys[i, isp]    += vya*fxc
@@ -130,11 +148,10 @@ def parmov():
             dns[i + 1,isp] += fx
 
             acc1 = acc2
-    return
+    return dns, vxs, vys, vzs
 
 
-def field():
-    hx2  = .5 * hx
+def field(den, vix, viy, viz):
     hxi2 = .5 * hxi
     #dtx  = dt * hxi2
     hxs  = hx * hx
@@ -150,63 +167,67 @@ def field():
     gc21 = 0.
     gc22 = 0.
 
-    e11  = np.zeros(nx)
-    e12  = np.zeros(nx)
-    e21  = np.zeros(nx)
-    e22  = np.zeros(nx)
-    f1   = np.zeros(nx)
-    f2   = np.zeros(nx)
-    g11  = np.zeros(nx)
-    g12  = np.zeros(nx)
-    g21  = np.zeros(nx)
-    g22  = np.zeros(nx)
-    h11  = np.zeros(nx)
-    h12  = np.zeros(nx)
-    h21  = np.zeros(nx)
-    h22  = np.zeros(nx)
+    e11  = np.zeros(nx + 2)
+    e12  = np.zeros(nx + 2)
+    e21  = np.zeros(nx + 2)
+    e22  = np.zeros(nx + 2)
+    f1   = np.zeros(nx + 2)
+    f2   = np.zeros(nx + 2)
+    g11  = np.zeros(nx + 2)
+    g12  = np.zeros(nx + 2)
+    g21  = np.zeros(nx + 2)
+    g22  = np.zeros(nx + 2)
+    h11  = np.zeros(nx + 2)
+    h12  = np.zeros(nx + 2)
+    h21  = np.zeros(nx + 2)
+    h22  = np.zeros(nx + 2)
 
-#  set up A B C D arrays (eqns. 49-57)
+    # Set up A B C D tridiagonal arrays 
     for i in range(1, nx):
-        df   = den[i] * resis / bxc
-        df1  = den[i] / (1. + df * df)
-        df2  = hx2 * vix[i] * df1 / bxc
-        df3  = hxs * df1
-        df4  = 1. / (dt*bxc)
-        a12  = -df2
-        a21  = -a12
-        a11  = 1.-df*a12
-        a22  = a11
-        b12  = df3*df4
+        df   = den[i] * resis / bxc                                 # Eq 5.43
+
+        a12  = -den[i]*hx*vix[i] / (2. * bxc * (1. + df ** 2))      # Eq 5.50                            
+        a11  = 1. - df*a12                                          # Eq 5.49
+        a21  = -a12                                                 
+        a22  =  a11
+        
+        b12  = (den[i] * hx ** 2) / (bxc * (1. + df ** 2) * dt)     # Eq 5.52
+        b11  = -2.-df*b12                                           # Eq 5.51
         b21  = -b12
-        b11  = -2.-df*b12
-        b22  = b11
-        c12  = -a12
+        b22  =  b11
+        
+        c12  = - a12                                                # Eq 5.54
+        c11  = 1.-df*c12                                            # Eq 5.53
         c21  = -c12
-        c11  = 1.-df*c12
-        c22  = c11
-        viyy = viy[i]-vix[i]*byc/bxc
-        vizz = viz[i]-vix[i]*bzc/bxc
-        d1   = -df3*(viyy+df*vizz-df4*(az[i]-df*ay[i]))
-        d2   = -df3*(vizz-df*viyy+df4*(ay[i]+df*az[i]))
+        c22  =  c11
+        
+        viyp = viy[i] - vix[i]*byc / bxc                            # Eq 5.56
+        vizp = viz[i] - vix[i]*bzc / bxc                            # Eq 5.57
+                                                 
+        df3  = hxs * den[i] / (1. + df * df)                        # Intermediaries
+        df4  = 1. / (dt*bxc)
+        
+        d1   = - df3*(viyp + df*vizp - df4*(az[i] - df*ay[i]))      # Eq 5.55a
+        d2   = - df3*(vizp - df*viyp + df4*(ay[i] + df*az[i]))      # Eq 5.55b
 
         if i == 1:
-            #  solve for E(2), F(2), G(2) (eqns. 67-69)
-            ddi    = 1. / (b11*b22-b12*b21)
+            #  Solve for E[1], F[1], G[1]
+            ddi    = 1. / (b11*b22 - b12*b21)
 
-            e11[1] = -ddi * ( b22 * c11 - b12 * c21)
+            e11[1] = -ddi * ( b22 * c11 - b12 * c21)                # Eq 5.67
             e12[1] = -ddi * ( b22 * c12 - b12 * c22)
             e21[1] = -ddi * (-b21 * c11 + b11 * c21)
             e22[1] = -ddi * (-b21 * c12 + b11 * c22)
 
-            f1[1]  =  ddi * ( b22 * d1  - b12 * d2 )
+            f1[1]  =  ddi * ( b22 * d1  - b12 * d2 )                # Eq 5.68
             f2[1]  =  ddi * (-b21 * d1  + b11 * d2 )
 
-            g11[1] = -ddi * ( b22 * a11 - b12 * a21)
+            g11[1] = -ddi * ( b22 * a11 - b12 * a21)                # Eq 5.69
             g12[1] = -ddi * ( b22 * a12 - b12 * a22)
             g21[1] = -ddi * (-b21 * a11 + b11 * a21)
             g22[1] = -ddi * (-b21 * a12 + b11 * a22)
         else:
-            # solve for E(I), F(I), G(I) (60-62); make EC, FC, GC arrays (74-76)
+            # Solve for E(I), F(I), G(I) (60-62); make EC, FC, GC arrays (74-76)
             h11 = a11 * e11[i - 1] + a12 * e21[i - 1] + b11
             h12 = a11 * e12[i - 1] + a12 * e22[i - 1] + b12
             h21 = a21 * e11[i - 1] + a22 * e21[i - 1] + b21
@@ -252,70 +273,70 @@ def field():
             ec21 = tc21
             ec22 = tc22
 
-    # solve for X(NX1) (77-78) : CHECKED
-        en11 = e11[nx - 1] + g11[nx - 1]
-        en12 = e12[nx - 1] + g12[nx - 1]
-        en21 = e21[nx - 1] + g21[nx - 1]
-        en22 = e22[nx - 1] + g22[nx - 1]
+    # Solve for X(NX) (77-78) : CHECKED
+    en11 = e11[nx - 1] + g11[nx - 1]
+    en12 = e12[nx - 1] + g12[nx - 1]
+    en21 = e21[nx - 1] + g21[nx - 1]
+    en22 = e22[nx - 1] + g22[nx - 1]
 
-        ec11 = ec11 + gc11
-        ec12 = ec12 + gc12
-        ec21 = ec21 + gc21
-        ec22 = ec22 + gc22
+    ec11 = ec11 + gc11
+    ec12 = ec12 + gc12
+    ec21 = ec21 + gc21
+    ec22 = ec22 + gc22
 
-        h11  = a11 * en11 + a12 * en21 + b11 + c11 * ec11 + c12 * ec21
-        h12  = a11 * en12 + a12 * en22 + b12 + c11 * ec12 + c12 * ec22
-        h21  = a21 * en11 + a22 * en21 + b21 + c21 * ec11 + c22 * ec21
-        h22  = a21 * en12 + a22 * en22 + b22 + c21 * ec12 + c22 * ec22
+    h11  = a11 * en11 + a12 * en21 + b11 + c11 * ec11 + c12 * ec21
+    h12  = a11 * en12 + a12 * en22 + b12 + c11 * ec12 + c12 * ec22
+    h21  = a21 * en11 + a22 * en21 + b21 + c21 * ec11 + c22 * ec21
+    h22  = a21 * en12 + a22 * en22 + b22 + c21 * ec12 + c22 * ec22
 
-        hdi =1. / (h11 * h22 - h12 * h21)
-        p1  = d1 -a11 * f1[nx - 1] - a12 * f2[nx - 1] - c11 * fc1 - c12 * fc2
-        p2  = d2 -a21 * f1[nx - 1] - a22 * f2[nx - 1] - c21 * fc1 - c22 * fc2
+    hdi  = 1. / (h11 * h22 - h12 * h21)
+    p1   = d1 -a11 * f1[nx - 1] - a12 * f2[nx - 1] - c11 * fc1 - c12 * fc2
+    p2   = d2 -a21 * f1[nx - 1] - a22 * f2[nx - 1] - c21 * fc1 - c22 * fc2
 
-        ey[nx] = ay[nx]
-        ez[nx] = az[nx]
-        ay[nx] = hdi * ( h22 * p1 - h12 * p2)
-        az[nx] = hdi * (-h21 * p1 + h11 * p2)
+    ey[nx] = ay[nx]
+    ez[nx] = az[nx]
+    ay[nx] = hdi * ( h22 * p1 - h12 * p2)
+    az[nx] = hdi * (-h21 * p1 + h11 * p2)
 
     # get all X(I) (58)
     for ii in range(1, nx - 1):
-        i = nx + 1 - ii
-        ey[i]=ay[i]
-        ez[i]=az[i]
-        ay[i]=e11[i]*ay[i + 1]+e12[i]*az[i + 1]+f1[i]+g11[i]*ay[nx]+g12[i]*az[nx]
-        az[i]=e21[i]*ay[i + 1]+e22(i)*az[i + 1]+f2[i]+g21[i]*ay[nx]+g22[i]*az[nx]
+        i     = nx - ii             # Addresses from NX to 2 (FORTRAN) in reverse (NX - 1, 1 in Python)
+        ey[i] = ay[i]
+        ez[i] = az[i]
+        ay[i] = e11[i]*ay[i + 1] + e12[i]*az[i + 1] + f1[i] + g11[i]*ay[nx] + g12[i]*az[nx]
+        az[i] = e21[i]*ay[i + 1] + e22[i]*az[i + 1] + f2[i] + g21[i]*ay[nx] + g22[i]*az[nx]
 
     ey[nx + 1] = ay[nx + 1]
     ez[nx + 1] = az[nx + 1]
     ay[nx + 1] = ay[1]
     az[nx + 1] = az[1]
-    ey[0] = ay[0]
-    ez[0] = az[0]
-    ay[0] = ay[nx]
-    az[0] = az[nx]
+    ey[0]      = ay[0]
+    ez[0]      = az[0]
+    ay[0]      = ay[nx]
+    az[0]      = az[nx]
 
-#  get Ey Ez (35-36); By Bz (31-32)
+    #  Get Ey Ez (35-36); By Bz (31-32)
     for i in range(1, nx):
         ey[i] = (ey[i] - ay[i]) / dt
         ez[i] = (ez[i] - az[i]) / dt
         by[i] = (az[i - 1] - az[i + 1]) * hxi2 + byc
         bz[i] = (ay[i + 1] - ay[i - 1]) * hxi2 + bzc
 
-        ey[1]      = ey[nx]
-        ez[1]      = ez[nx]
-        ey[nx + 1] = ey[1]
-        ez[nx + 1] = ez[1]
-        by[nx + 1] = by[1]
-        bz[nx + 1] = bz[1]
-        by[1]      = by[nx]
-        bz[1]      = bz[nx]
+    ey[0]      = ey[nx]
+    ez[0]      = ez[nx]
+    ey[nx + 1] = ey[1]
+    ez[nx + 1] = ez[1]
+    by[nx + 1] = by[1]
+    bz[nx + 1] = bz[1]
+    by[0]      = by[nx]
+    bz[0]      = bz[nx]
 
-    # calculate del**2 A, electron velocities, drag force
+    # Calculate del**2 A, electron velocities, drag force
     for i in range(1, nx):
         ajy    = -(ay[i + 1] + ay[i + 1] - 2. * ay[i]) / hxs
         ajz    = -(az[i + 1] + az[i + 1] - 2. * az[i]) / hxs
-        vey[i] = -ajy / (den[i]) + viy[i]
-        vez[i] = -ajz / (den[i]) + viz[i]
+        vey[i] = -ajy / den[i] + viy[i]
+        vez[i] = -ajz / den[i] + viz[i]
         foy[i] = -resis*ajy
         foz[i] = -resis*ajz
 
@@ -327,23 +348,24 @@ def field():
             te[i] = te0 * (den[i]**gam)
         pe[i]   = te[i]*den[i]
 
-    vey[nx + 1]= vey[1]
-    vez[nx + 1]= vez[1]
-    te[nx + 1] = te[1]
-    pe[nx + 1] = pe[1]
+    vey[nx + 1] = vey[1]
+    vez[nx + 1] = vez[1]
+    te[ nx + 1] = te[1]
+    pe[ nx + 1] = pe[1]
+    
     vey[0]     = vey[nx]
     vez[0]     = vez[nx]
-    te[0]      = te[nx]
-    pe[0]      = pe[nx]
+    te[0]      = te[ nx]
+    pe[0]      = pe[ nx]
 
-    # calculate Ex
+    # Calculate Ex
     for i in range(1, nx):
         ex[i] = vez[i] * by[i] - vey[i] * bz[i]
         ex[i] = ex[i] - hxi2 * (pe[i + 1] - pe[i - 1]) / den[i]
 
     ex[0]      = ex[nx]
     ex[nx + 1] = ex[1]
-    return
+    return by, bz, ex, ey, ez
 
 
 if __name__ == '__main__':
@@ -416,14 +438,14 @@ if __name__ == '__main__':
 
     te  = np.ones(nx + 2) * te0        # Electron temperature
     pe  = np.ones(nx + 2) * pe0        # Electron pressure
+    
+    foy = np.zeros(nx + 2)             # Force?
+    foz = np.zeros(nx + 2)             # Force?
     by  = np.ones(nx + 2) * byc        # Magnetic field: y
     bz  = np.ones(nx + 2) * bzc        # Magnetic field: z
-
     ex  = np.zeros(nx + 2)             # Electric field: x
     ey  = np.zeros(nx + 2)             # Electric field: y
     ez  = np.zeros(nx + 2)             # Electric field: z
-    foy = np.zeros(nx + 2)             #
-    foz = np.zeros(nx + 2)
 
     vxs = np.zeros((nx + 2, nsp))      # Source array: Velocity x
     vys = np.zeros((nx + 2, nsp))      # Source array: Velocity y
@@ -465,7 +487,7 @@ if __name__ == '__main__':
             th       = 2.*np.pi*np.random.rand()                        # Random angle
             vxa      = vth[isp]*vmag*np.cos(th) + vbx[isp]              # Particle x-velocity plus bulk velocity
 
-            vmag     = np.sqrt(-np.log(1.-.999999*np.random.rand()))     # Velocity magnitude (Maxwellian distribution)
+            vmag     = np.sqrt(-np.log(1.-.999999*np.random.rand()))    # Velocity magnitude (Maxwellian distribution)
             th       = 2.*np.pi*np.random.rand()                        # Random angle
             vy[ii]   = vth[isp]*ans[isp]*vmag*np.sin(th)                # Particle y-velocity
             vza      = vth[isp]*ans[isp]*vmag*np.cos(th)                # Particle z-velocity
@@ -480,14 +502,15 @@ if __name__ == '__main__':
     # Run one time step with dt=0 to initialize fields
     dtsav = dt
     dt    = 0.
-    trans()
+    den, vix, viy, viz  = trans(by, bz, ex, ey, ez)
     dt    = dtsav
-    field()
-
+    by, bz, ex, ey, ez  = field(den, vix, viy, viz)
     it = 0; t = 0
+
     while it <= ntimes:
+        print 'iteration {}'.format(it)
         it += 1
         t  += dtwci
 
-        trans()
-        field()
+        den, vix, viy, viz = trans(by, bz, ex, ey, ez)
+        by, bz, ex, ey, ez = field(den, vix, viy, viz)
