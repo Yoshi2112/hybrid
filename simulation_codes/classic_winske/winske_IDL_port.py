93280eee4bddfe9cb8d0765ef018548357824f69
Yoshi2112 <joshua.s.williams@uon.edu.au>
2018-12-05 13:28:02 +1100

    latest

--- simulation_codes/classic_winske/winske_IDL_port.py
+++ simulation_codes/classic_winske/winske_IDL_port.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gs
 
 # =============================================================================
 # ;I THINK I MADE A BIG MISTAKE
@@ -425,12 +427,108 @@ def field():
     return
 
 
+def winske_stackplot(qq, time):
+#----- Prepare some values for plotting
+    x_cell_num  = np.arange(nx)                                         # Numerical cell numbering: x-axis
+    phi         = np.arctan2(Bz[2:nx2], By[2:nx2]) + np.pi              # Wave magnetic phase angle
+    
+#----- Create plots
+    plt.ioff()
+    fig    = plt.figure(1, figsize=(8.27, 11.69))                       # Initialize figure
+    grids  = gs.GridSpec(5, 1)                                          # Create gridspace
+    fig.patch.set_facecolor('w')                                        # Set figure face color
+
+    ax_vx   = fig.add_subplot(grids[0, 0]) 
+    ax_vy   = fig.add_subplot(grids[1, 0]) 
+    ax_den  = fig.add_subplot(grids[2, 0])                              # Initialize axes
+    ax_by   = fig.add_subplot(grids[3, 0]) 
+    ax_phi  = fig.add_subplot(grids[4, 0]) 
+
+    ax_vx.scatter(x[:, 1], vx[:, 1]*1e3, s=1, c='k', lw=0)              # Hot population
+    ax_vy.scatter(x[:, 1], vy[:, 1]*1e3, s=1, c='k', lw=0)              # 'Other' population
+    
+    ax_den.plot(x_cell_num, dns[2:nx2, 1], c='k')                       # Create overlayed plots for densities of each species
+    ax_by.plot(x_cell_num, 10*By[2:nx2]/Bxc, c='k')
+    ax_phi.plot(x_cell_num, phi, c='k')
+
+    ax_vx.set_ylim(- 1.41 , 1.41) 
+    ax_vy.set_ylim(- 1.41 , 1.41) 
+    ax_den.set_ylim( 0.71 , 1.39)                                       # Initialize axes
+    ax_by.set_ylim(- 5.99 , 4.55) 
+    ax_phi.set_ylim( 0.01 , 6.24) 
+
+    ax_vx.set_yticks( [-1.41, -0.71, 0.00, 0.71, 1.41])
+    ax_vy.set_yticks( [-1.41, -0.71, 0.00, 0.71, 1.41])
+    ax_den.set_yticks([0.71, 0.88, 1.05, 1.22, 1.39])
+    ax_by.set_yticks( [-5.99, -3.36, -0.72, 1.91, 4.55])
+    ax_phi.set_yticks([0.01, 1.57, 3.13, 4.68, 6.24])
+
+    ax_vx.set_ylabel('VX ($x 10^{-3}$)', rotation=90)
+    ax_vy.set_ylabel('VY ($x 10^{-3}$)', rotation=90)
+    ax_den.set_ylabel('DNB', rotation=90)
+    ax_by.set_ylabel('BY ($x 10^{-1}$)', rotation=90)
+    ax_phi.set_ylabel('PHI', rotation=90)
+
+    ax_phi.set_xlim(0, 128)
+    ax_phi.set_xlabel('X (CELL)')
+
+    plt.setp(ax_vx.get_xticklabels(), visible=False)
+    #ax_vx.set_yticks(ax_vx.get_yticks()[1:])
+
+    for ax in [ax_vx, ax_vy, ax_den, ax_by]:
+        plt.setp(ax.get_xticklabels(), visible=False)
+        #ax.set_yticks(ax.get_yticks()[1:])
+        ax.set_xlim(0, 128)
+
+    #plt.tight_layout(pad=1.0, w_pad=1.8)
+    fig.subplots_adjust(hspace=0.1)
+
+    fig.text(0.42, 0.045, 'IT = {}'.format(qq), fontsize=13)    
+    fig.text(0.58, 0.045, 'T = %.2f' % (time), fontsize=13)
+    
+#----- Save plots
+    filename = 'stackplot%05d.png' % qq
+    fullpath = save_path + '/stackplot/' + filename
+    plt.savefig(fullpath, facecolor=fig.get_facecolor(), edgecolor='none')
+    plt.close('all')
+    return
+
+
 if __name__ == '__main__':
 ### DEFINE INPUT VARIABLES
+# =============================================================================
+#     input files for test runs
+# 
+#     $datum ntimes=2001, dtwci=0.05, nx=128, xmax=256.,
+#     npltp=200, npltf=200, nplth=1000, nwrtf=20, nwrth=40,
+#     wpiwci=10000., nsp=2, nspec=5120,5120, vbspec=9.85,-0.15,
+#     dnspec=0.015,0.985, btspec=1.,1., anspec=1.,1., wspec=1.,1.,
+#     bete=1., resis=0., theta=0., iemod=0, nskip=2,
+#     label='1-d hybrid test 1: resonant instability'
+#    
+#     $datum ntimes=2001, dtwci=0.025, nx=128, xmax=128.,
+#     npltp=200, npltf=200, nplth=1000, nwrtf=20, nwrth=40,
+#     wpiwci=10000., nsp=2, nspec=5120,5120, vbspec=9.00,-1.00,
+#     dnspec=0.10,0.900, btspec=1.,1., anspec=1.,1., wspec=1.,1.,
+#     bete=1., resis=0., theta=0., iemod=0, nskip=2,
+#     label='1-d hybrid test 2: nonresnt instability'
+#    
+#     $datum ntimes=1001, dtwci=0.05, nx=128, xmax=128.,
+#     npltp=200, npltf=200, nplth=1000, nwrtf=20, nwrth=40,
+#     wpiwci=10000., nsp=2, nspec=5120,5120, vbspec=0.90,-0.10,
+#     dnspec=0.10,0.900, btspec=10.,1., anspec=5.,1., wspec=1.,1.,
+#     bete=1., resis=0., theta=0., iemod=0, nskip=2,
+#     label='1-d hybrid test 3: anistrpy instability'
+# =============================================================================
+    
     t        = 0               # i added this, NOT IN WINSKE
     
+    plt.ioff()
+    plot     = True
+    save_path = 'E://runs//winske//'
+    
     # General
-    ntimes    = 2001           # time steps
+    ntimes    = 1001           # time steps
     dtwci     = 0.05           # dt in wci units
     nx        = 128            # no of computational cells (not including two ghost cells)
     xmax      = 128.           # system length in c/wpi units
@@ -441,10 +539,10 @@ if __name__ == '__main__':
     # Ions
     nsp       = 2
     nspec     = [5120,5120]    # No of simulation particles of each species
-    vbspec    = [9.00,-1.00]   # Velocity for each species in alfven speed units (va^2=Bo^2/(4*pi*no*mo) see pg 120)
+    vbspec    = [0.9, -0.1]    # Velocity for each species in alfven speed units (va^2=Bo^2/(4*pi*no*mo) see pg 120)
     dnspec    = [0.10,0.900]   # Total density of each species
-    btspec    = [100.,1.]      # Plasma beta for each species
-    anspec    = [1.,21.]       # Anisotropy: T-perp/T-parallel for each species (?? see original code and pg 121?)
+    btspec    = [10.,1.]       # Plasma beta for each species
+    anspec    = [5.,1.]        # Anisotropy: T-perp/T-parallel for each species (?? see original code and pg 121?)
     wspec     = [1.,1.]        # Mass of each species in proton mass units
     
     # Electrons
@@ -585,7 +683,7 @@ if __name__ == '__main__':
         pe[i]    = pe0
         By[i]    = Byc
         Bz[i]    = Bzc
-    
+
     #now we run one time step with dt = 0 to initialise the fields
     dtsav    = dt
     dt       = 0.
@@ -597,8 +695,105 @@ if __name__ == '__main__':
 
 ### MAIN LOOP
     for it in range(1, ntimes + 1):
+        t        = t  + dtwci        # THERES SOMETHING STRANGE ABOUT THE DT AND DTWCI HERE??
         print 'Iteration {}, time = {}'.format(it, t)
-        t        = t  + dtwci        #THERES SOMETHING STRANGE ABOUT THE DT AND DTWCI HERE??
-    
+        
         trans()
         field()
+        
+        if plot == True and round(t, 2)%1 == 0:
+            print 'Ploting...'
+            
+            winske_stackplot(it, t)
+            
+# =============================================================================
+#             # E-Field
+#             fig1 = plt.figure()
+#             
+#             ax1 = plt.subplot2grid((3, 3), (0, 0), colspan=3)
+#             ax2 = plt.subplot2grid((3, 3), (1, 0), colspan=3)
+#             ax3 = plt.subplot2grid((3, 3), (2, 0), colspan=3)
+#             
+#             ax1.set_title(r'Electric Fields ($nV/m$) $\Omega t$ = {}'.format(t))
+#             ax1.plot(Ex * 1e9)
+#             ax1.set_ylabel('$E_x \times 10^9$')
+#             #ax1.set_ylim(-50, 50)
+#             
+#             ax2.plot(Ey * 1e9)
+#             ax2.set_ylabel('$E_y \times 10^9$')
+#             #ax2.set_ylim(-100, 100)
+#             
+#             ax3.plot(Ez * 1e9)
+#             ax3.set_ylabel('$E_z \times 10^9$')
+#             #ax3.set_ylim(-100, 100)
+#             
+#             for ax in [ax1, ax2, ax3]:
+#                 ax.set_xlim(0, 130)
+#             
+#             filename = 'Efields_{}.png'.format(it)
+#             fullpath = save_path + '//E//'+ filename
+#             plt.savefig(fullpath, facecolor=fig1.get_facecolor(), edgecolor='none')
+#             plt.close('all')
+#             
+#             # B-Field
+#             fig2 = plt.figure()
+#             
+#             ax1 = plt.subplot2grid((3, 3), (0, 0), colspan=3)
+#             ax2 = plt.subplot2grid((3, 3), (1, 0), colspan=3)
+#             ax3 = plt.subplot2grid((3, 3), (2, 0), colspan=3)
+#             
+#             ax1.set_title(r'Magnetic Fields $\Omega t$ = {}'.format(t))
+#             ax1.plot(np.sqrt(Bxc ** 2 + By ** 2 + Bz ** 2) / Bxc)
+#             ax1.set_ylabel('$|B| / B_0$')
+#             ax1.set_ylim(0, 2)
+#             
+#             ax2.plot(By / Bxc)
+#             ax2.set_ylabel('$B_y / B_0$')
+#             ax2.set_ylim(-1, 1)
+#             
+#             ax3.plot(Bz / Bxc)
+#             ax3.set_ylabel('$B_z / B_0$')
+#             ax3.set_ylim(-1, 1)
+#             
+#             for ax in [ax1, ax2, ax3]:
+#                 ax.set_xlim(0, 130)
+#             
+#             filename = 'Bfields_{}.png'.format(it)
+#             fullpath = save_path + '//B//' + filename
+#             plt.savefig(fullpath, facecolor=fig2.get_facecolor(), edgecolor='none')
+#             plt.close('all')
+# 
+# 
+#             # Velocity
+#             fig3 = plt.figure()
+#             
+#             ax1 = plt.subplot2grid((4, 4), (0, 0), colspan=4)
+#             ax2 = plt.subplot2grid((4, 4), (1, 0), colspan=4)
+#             ax3 = plt.subplot2grid((4, 4), (2, 0), colspan=4)
+#             ax4 = plt.subplot2grid((4, 4), (3, 0), colspan=4)
+# 
+#             ax1.set_title(r'Moments (D = {}) $\Omega t$ = {}'.format(den[1:-1].sum(), t))
+#             ax1.plot(den)
+#             ax1.set_ylabel('$n_i$')
+#             ax1.set_ylim(0, 4)
+#             
+#             ax2.plot(Vix)
+#             ax2.set_ylabel('$v_x / c$')
+#             #ax2.set_ylim(-1e-3, 1e-3)
+#             
+#             ax3.plot(Viy)
+#             ax3.set_ylabel('$v_y / c$')
+#             #ax3.set_ylim(-1e-4, 1e-4)
+#             
+#             ax4.plot(Viz)
+#             ax4.set_ylabel('$v_z / c$')
+#             #ax4.set_ylim(-1e-4, 1e-4)
+#             
+#             for ax in [ax1, ax2, ax3, ax4]:
+#                 ax.set_xlim(0, 130)
+#                 
+#             filename = 'moments_{}.png'.format(it)
+#             fullpath = save_path + '//M//' + filename
+#             plt.savefig(fullpath, facecolor=fig3.get_facecolor(), edgecolor='none')
+#             plt.close('all')
+# =============================================================================
