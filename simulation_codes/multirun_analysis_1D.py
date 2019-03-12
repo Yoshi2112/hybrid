def06e3628f06555ab5b751965505201a802ff7c
Yoshi2112 <joshua.s.williams@uon.edu.au>
2019-03-12 11:41:49 +1100

    Upgraded analysis codes

--- simulation_codes/multirun_analysis_1D.py
+++ simulation_codes/multirun_analysis_1D.py
@@ -6,6 +6,7 @@ Created on Wed Apr 27 11:56:34 2016
 """
 
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import os
 from numpy import pi
@@ -13,6 +14,7 @@ import pickle
 import numba as nb
 from collections import OrderedDict
 from matplotlib.lines import Line2D
+
 import pdb
 
 
@@ -110,6 +112,14 @@ def load_header():
     return 
 
 
+def create_idx():
+    N_part = cellpart * NX
+    idx    = np.zeros(N_part, dtype=int)
+    
+    for jj in range(Nj):
+        idx[idx_bounds[jj, 0]: idx_bounds[jj, 1]] = jj
+    return idx
+
 def load_timestep(ii):
     print 'Loading file {} of {}'.format(ii+1, num_files)
     d_file     = 'data%05d.npz' % ii                # Define target file
@@ -124,8 +134,8 @@ def load_timestep(ii):
     tpos             = data['pos']
     tdns             = data['dns']
     tvel             = data['vel']
-
-    return tB, tE, tVe, tTe, tJ, tpos, tdns, tvel
+    tidx             = create_idx()
+    return tB, tE, tVe, tTe, tJ, tpos, tdns, tvel, tidx
 
 
 @nb.njit()
@@ -136,28 +146,75 @@ def collect_number_density(pos):
     INPUT:
         pos    -- position of each particle
     '''
-    size      = NX + 2
-
-    n_i       = np.zeros((size, Nj))
+    @nb.njit()
+    def manage_ghost_cells(arr):
+        '''Deals with ghost cells: Moves their contributions and mirrors their counterparts.
+           Works like a charm if spatial dimensions always come first in an array.'''
     
-    node      = pos / dx + 0.5 
-    weight    = (pos / dx) - node + 0.5
-    n_contr    = density / (cellpart*sim_repr)      # Density: initial /m3 of species in cell, divide this by number of particles in the cell
-                                                    # Each macroparticle contributes this amount to the cell's density in /m3
-    for jj in range(Nj):
-        for ii in range(idx_bounds[jj, 0], idx_bounds[jj, 1]):
-            I   = int(node[ii])
-            W   = weight[ii]
-
-            n_i[I,     jj] += (1 - W) * n_contr[jj]
-            n_i[I + 1, jj] +=      W  * n_contr[jj]
-
-    n_i[1]  += n_i[-1]
-    n_i[-1] += n_i[0]
+        arr[NX]     += arr[0]                 # Move contribution: Start to end
+        arr[1]      += arr[NX + 1]            # Move contribution: End to start
     
-    n_i[0]   = 0.
-    n_i[-1]  = 0
-    return n_i
+        arr[NX + 1]  = arr[1]                 # Fill ghost cell: End
+        arr[0]       = arr[NX]                # Fill ghost cell: Start
+        
+        arr[NX + 2]  = arr[2]                 # This one doesn't get used, but prevents nasty nan's from being in array.
+        return arr
+
+    @nb.njit()
+    def assign_weighting_TSC(pos, E_nodes=True):
+        '''Triangular-Shaped Cloud (TSC) weighting scheme used to distribute particle densities to
+        nodes and interpolate field values to particle positions.
+    
+        INPUT:
+            pos  -- particle positions (x)
+            BE   -- Flag: Weighting factor for Magnetic (0) or Electric (1) field node
+            
+        OUTPUT:
+            weights -- 3xN array consisting of leftmost (to the nearest) node, and weights for -1, 0 TSC nodes
+        '''
+        Np         = pos.shape[0]
+        
+        left_node  = np.zeros(Np,      dtype=np.uint16)
+        weights    = np.zeros((3, Np), dtype=np.float64)
+        
+        if E_nodes == True:
+            grid_offset   = 0.5
+        else:
+            grid_offset   = 1.0
+        
+        for ii in nb.prange(Np):
+            left_node[ii]  = int(round(pos[ii] / dx + grid_offset) - 1.0)
+            delta_left     = left_node[ii] - pos[ii] / dx - grid_offset
+        
+            weights[0, ii] = 0.5  * np.square(1.5 - abs(delta_left))
+            weights[1, ii] = 0.75 - np.square(delta_left + 1.)
+            weights[2, ii] = 1.0  - weights[0, ii] - weights[1, ii]
+        return left_node, weights
+
+    @nb.njit()
+    def collect_moments(vel, Ie, W_elec, idx):
+        n_contr   = density / (cellpart*sim_repr)
+        size      = NX + 3
+        n_i       = np.zeros((size, Nj))
+        
+        for ii in nb.prange(vel.shape[1]):
+            I   = Ie[ ii]
+            sp  = idx[ii]
+            
+            n_i[I,     sp] += W_elec[0, ii]
+            n_i[I + 1, sp] += W_elec[1, ii]
+            n_i[I + 2, sp] += W_elec[2, ii]
+            
+        for jj in range(Nj):
+            n_i[:, jj] *= n_contr[jj]
+    
+        n_i   = manage_ghost_cells(n_i)
+        return n_i
+
+    left_node, weights  = assign_weighting_TSC(pos, E_nodes=True) 
+    idx                 = create_idx()
+    den                 = collect_moments(left_node, weights, idx)   
+    return den
 
 
 def get_array(component, tmin, tmax):
@@ -184,12 +241,12 @@ def get_array(component, tmin, tmax):
         arr = np.load(check_path)   
     else:
         for ii in range(tmin, tmax):
-            B, E, Ve, Te, J, position, q_dns, velocity = load_timestep(ii)
+            B, E, Ve, Te, J, position, q_dns, velocity, idx = load_timestep(ii)
             
             if component[0].upper() == 'B':
-                arr[ii] = B[0:-1, comp_idx]
+                arr[ii] = B[1:-2, comp_idx]
             elif component[0].upper() == 'E':
-                arr[ii] = E[1:-1, comp_idx]
+                arr[ii] = E[1:-2, comp_idx]
                 
         print 'Saving array file as {}'.format(check_path)
         np.save(check_path, arr)
@@ -222,7 +279,7 @@ def get_run_energies():
     energy_output[0] = time_gperiods        
     
     for ii in range(num_files):
-        B, E, Ve, Te, J, position, q_dns, velocity = load_timestep(ii)
+        B, E, Ve, Te, J, position, q_dns, velocity, idx = load_timestep(ii)
             
         energy_output[2, ii] = (0.5 / mu0) * np.square(B[1:-2]).sum() * NX * dx    # Magnetic potential energy 
         energy_output[3, ii] = 1.5 * (kB * Te * q_dns / q).sum()      * NX * dx    # Electron pressure energy
@@ -274,61 +331,157 @@ def plot_energies(energy, ax, normalize=True):
     return
 
 
+def plot_growth_grid(plot_ratio=True, norm=False):
+    '''
+    Density on x axis
+    Field (B0) on y axis
+    Growth (max b1) coded in color
+    '''
+    if norm == False:
+        add  = '(nT)'
+    else:
+        add  = r'/$B_0$'
+        
+    # Turn field values into colors to plot
+    cmap      = matplotlib.cm.get_cmap('nipy_spectral')
+    normalize = matplotlib.colors.Normalize(vmin=0, vmax=max_field[2].max())
+    colors    = [cmap(normalize(value)) for value in max_field[2]]
+    
+    # Plot model values
+    fig, ax = plt.subplots(figsize=(15,10))
+    ax.scatter(max_field[0], max_field[1], color=colors)
+    
+    if plot_ratio == True:
+        npts = 100
+        rat  = np.sqrt(e0 / mp)
+        
+        B_min  = 0e-9
+        B_max  = 300e-9
+        B_axis = np.linspace(B_min, B_max, npts)
+        
+        powers = range(1, 5)
+        for pwr in powers:
+            #sqrt_n = (rat * (10 ** pwr) * B_axis) / 1e3
+            n      = (rat * (10 ** pwr) * B_axis) ** 2 / 1e6
+            plt.plot(n, B_axis*1e9, label='$cv_A^{-1} = 10^%d$' % pwr)
+    
+    ## PLOT DATA POINTS ##
+    n_data = np.array([38, 160, 38, 160])
+    B_data = np.array([158, 158, 134, 134])
+    plt.scatter(n_data, B_data, label='Event Data Limits', marker='x')
+    plt.legend()
+    
+    ## LABELS AND LIMITS ##
+    plt.xlim(0, 500)
+    plt.ylim(B_min*1e9, B_max*1e9)
+    
+    plt.xlabel(r'$n_0 ({cm^{-3}})$')
+    plt.ylabel(r'$B_0 (nT)$')
+    
+
+        
+    plt.title('Event 1: Max |By|{} for varying B0, ne'.format(add))
+    
+    # Optionally add a colorbar
+    cax, _ = matplotlib.colorbar.make_axes(ax)
+    cbar   = matplotlib.colorbar.ColorbarBase(cax, cmap=cmap, norm=normalize)
+    cbar.set_label('|By|{}'.format(add), rotation=0)
+    
+    plt.show()
+    return
+
+
 if __name__ == '__main__':   
     plt.ioff()
     
-    drive           = 'E:'
-    series          = 'PC_CAMCL_better'                         # Run identifier string 
+    drive      = 'E:'
+    series     = 'Box_test_ev1_H_only'                          # Run identifier string 
+    series_dir = 'F://runs//{}//'.format(series)
+    num_runs   = len(os.listdir(series_dir))
     
-    runs_to_analyse = [0   ,  1   ,  2 ]                        # Run number
-    run_styles      = ['-' , '--' , ':']
+    max_field  = np.zeros((3, num_runs))                        # B0, n0, max_By
     
-    run_labels      = ['Predictor-Corrector',
-                       'CAM-CL' ,
-                       r'Pred-Corr equivalent $\Delta t$']
-    
-    total_energies  = np.zeros(len(runs_to_analyse))
-    
-    fig  = plt.figure(figsize=(15, 7))
-    ax   = plt.subplot2grid((7, 7), (0, 0), colspan=6, rowspan=7)
-    run_legend, ax  = create_legend(ax)
-        
-    fsize = 10; fname='monospace'; left = 0.86
-    plt.figtext(left, 0.50, r'TOTAL ENERGY CONSERVATION',    fontsize=fsize, fontname=fname)
-    plt.figtext(left, 0.50,  '_________________________',     fontsize=fsize, fontname=fname)
-
-    for run_num, ii in zip(runs_to_analyse, range(len(runs_to_analyse))):
+    for run_num in range(num_runs):
         manage_dirs()                                           # Initialize directories
         load_constants()                                        # Load SI constants
         load_header()                                           # Load simulation parameters
         load_particles()                                        # Load particle parameters
         num_files = len(os.listdir(data_dir)) - 2               # Number of timesteps to load
         
-        wpi       = np.sqrt(ne * q ** 2 / (mp * e0))            # Ion plasma frequency
-        gyfreq    = q * B0 / mp                                 # Proton gyrofrequency (rad/s)
-        gyperiod  = (mp * 2 * np.pi) / (q * B0)                 # Proton gyroperiod (s)
-        data_ts   = data_dump_iter * dt                         # Timestep between data records (seconds)
-               
-        print 'Analysing run {}'.format(run_num)
-        energies = get_run_energies()
-
-        plot_energies(energies, ax)
+        BY = get_array('By', 0, None)
         
-        total_energy_change = 100.*(energies[1, -1] - energies[1, -0]) / energies[1, 0]
-        plt.figtext(left, 0.475-ii*0.02, '{:>8} : {:>7}%'.format(method_type, round(total_energy_change, 2)),  fontsize=fsize,  fontname=fname)
-
-    fig.tight_layout()
-    
-    handles, labels = plt.gca().get_legend_handles_labels()
-    by_label = OrderedDict(zip(labels, handles))
-    ax.legend(by_label.values(), by_label.keys(), loc='center left', bbox_to_anchor=(1, 0.8))
-
-    ax.add_artist(run_legend)
-
+        max_field[0, run_num] = ne / 1e6                        # Density in cc
+        max_field[1, run_num] = B0 * 1e9                        # B0 in nT
+        max_field[2, run_num] = abs(BY).max() * 1e9             # Max abs(By) in nT
         
-    fullpath = base_dir + 'energy_plot'
-    ax.set_ylim(0.85, 1.4)
-    ax.set_xlim(0, 25)
-    fig.savefig(fullpath, facecolor=fig.get_facecolor(), edgecolor='none')
-    plt.close('all')
+    plot_growth_grid(norm=True)
+
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    compare_plots = False
+    if compare_plots == True:
+        runs_to_analyse = [0   ,  1   ,  2 ]                        # Run number
+        run_styles      = ['-' , '--' , ':']
+        
+        run_labels      = ['Predictor-Corrector',
+                           'CAM-CL' ,
+                           r'Pred-Corr equivalent $\Delta t$']
+        
+        total_energies  = np.zeros(len(runs_to_analyse))
+        
+        fig  = plt.figure(figsize=(15, 7))
+        ax   = plt.subplot2grid((7, 7), (0, 0), colspan=6, rowspan=7)
+        run_legend, ax  = create_legend(ax)
+            
+        fsize = 10; fname='monospace'; left = 0.86
+        plt.figtext(left, 0.50, r'TOTAL ENERGY CONSERVATION',    fontsize=fsize, fontname=fname)
+        plt.figtext(left, 0.50,  '_________________________',     fontsize=fsize, fontname=fname)
+    
+        for run_num, ii in zip(runs_to_analyse, range(len(runs_to_analyse))):
+            manage_dirs()                                           # Initialize directories
+            load_constants()                                        # Load SI constants
+            load_header()                                           # Load simulation parameters
+            load_particles()                                        # Load particle parameters
+            num_files = len(os.listdir(data_dir)) - 2               # Number of timesteps to load
+            
+            wpi       = np.sqrt(ne * q ** 2 / (mp * e0))            # Ion plasma frequency
+            gyfreq    = q * B0 / mp                                 # Proton gyrofrequency (rad/s)
+            gyperiod  = (mp * 2 * np.pi) / (q * B0)                 # Proton gyroperiod (s)
+            data_ts   = data_dump_iter * dt                         # Timestep between data records (seconds)
+                   
+            print 'Analysing run {}'.format(run_num)
+            energies = get_run_energies()
+    
+            plot_energies(energies, ax)
+            
+            total_energy_change = 100.*(energies[1, -1] - energies[1, -0]) / energies[1, 0]
+            plt.figtext(left, 0.475-ii*0.02, '{:>8} : {:>7}%'.format(method_type, round(total_energy_change, 2)),  fontsize=fsize,  fontname=fname)
+    
+        fig.tight_layout()
+        
+        handles, labels = plt.gca().get_legend_handles_labels()
+        by_label = OrderedDict(zip(labels, handles))
+        ax.legend(by_label.values(), by_label.keys(), loc='center left', bbox_to_anchor=(1, 0.8))
+    
+        ax.add_artist(run_legend)
+    
+            
+        fullpath = base_dir + 'energy_plot'
+        ax.set_ylim(0.85, 1.4)
+        ax.set_xlim(0, 25)
+        fig.savefig(fullpath, facecolor=fig.get_facecolor(), edgecolor='none')
+        plt.close('all')
+        
