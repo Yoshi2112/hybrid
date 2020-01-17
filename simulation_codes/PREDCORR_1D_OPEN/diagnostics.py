Fixed boundary issues, verified derivatives + grid working fine

simulation_codes/PREDCORR_1D_OPEN/diagnostics.py
@@ -461,6 +461,9 @@ def test_force_interpolation():
 
 
 def test_curl_B():
+    '''
+    Confirmed
+    '''
     NX   = 50     
     ND   = 10
     NC   = NX + 2*ND
@@ -468,51 +471,54 @@ def test_curl_B():
     xmax = 2*np.pi
     
     dx   = xmax / NX
-    k    = 2.0
+    k    = 1.0
 
-    # Physical location of nodes
-    E_nodes  = (np.arange(NC) - ND  + 0.5) * dx
-    B_nodes  = (np.arange(NC) - ND  - 0.0) * dx
-    
+    E_nodes  = (np.arange(NC    ) - ND  + 0.5) * dx
+    B_nodes  = (np.arange(NC + 1) - ND  - 0.0) * dx
+
     # Inputs and analytic solutions
-    B_input       = np.zeros((NC, 3))
+    B_input       = np.zeros((NC + 1, 3))
     B_input[:, 0] = np.cos(1.0*k*B_nodes)
     B_input[:, 1] = np.cos(1.5*k*B_nodes)
     B_input[:, 2] = np.cos(2.0*k*B_nodes)
     
     curl_B_anal       = np.zeros((NC, 3))
-    curl_B_anal[:, 1] = -1.5 * k * np.sin(1.5*k*E_nodes) / dx
-    curl_B_anal[:, 2] = -2.0 * k * np.sin(2.0*k*E_nodes) / dx
+    curl_B_anal[:, 1] =  2.0 * k * np.sin(2.0*k*E_nodes) * dx
+    curl_B_anal[:, 2] = -1.5 * k * np.sin(1.5*k*E_nodes) * dx
     
     curl_B_FD = np.zeros((NC, 3))
-    fields.curl_B_term(B_input, curl_B_FD, DX=dx)
+    fields.curl_B_term(B_input, curl_B_FD)
     
     ## DO THE PLOTTING ##
     plt.figure(figsize=(15, 15))
     marker_size = None
-    
-    #plt.plot(x, -deriv, linestyle=':', c='b', label='By Analytic Solution')
-    #plt.scatter(E_nodes, curl_B_anal[:, 1], marker='o', c='k', s=marker_size, label='By Node Solution')
-    plt.scatter(E_nodes, curl_B_FD[:, 1], marker='x', c='b', s=marker_size, label='By Finite Difference')
-    
-    #plt.plot(x, deriv, linestyle=':', c='r', label='Bz Analytic Solution')   
-    #plt.scatter(E_nodes, curl_B_anal[:, 2], marker='o', c='k', s=marker_size, label='Bz Node Solution')
-    plt.scatter(E_nodes, curl_B_FD[:, 2], marker='x', c='r', s=marker_size, label='Bz Finite Difference')   
+        
+    plt.scatter(E_nodes, curl_B_anal[:, 1], marker='o', c='k', s=marker_size, label='By Node Solution')
+    plt.scatter(E_nodes, curl_B_FD[  :, 1], marker='x', c='b', s=marker_size,   label='By Finite Difference')
+      
+    plt.scatter(E_nodes, curl_B_anal[:, 2], marker='o', c='k', s=marker_size, label='Bz Node Solution')
+    plt.scatter(E_nodes, curl_B_FD[  :, 2], marker='x', c='r', s=marker_size,   label='Bz Finite Difference')   
     plt.title(r'Test of $\nabla \times B$')
 
-    for kk in range(NC):
-        plt.axvline(E_nodes[kk], linestyle='--', c='r', alpha=0.2)
-        plt.axvline(B_nodes[kk], linestyle='--', c='b', alpha=0.2)
-        
-        plt.axvline(xmin, linestyle='-', c='k', alpha=0.2)
-        plt.axvline(xmax, linestyle='-', c='k', alpha=0.2)
+    for ii in range(E_nodes.shape[0]):
+        plt.axvline(E_nodes[ii], linestyle='--', c='r', alpha=0.2)
+        plt.axvline(B_nodes[ii], linestyle='--', c='b', alpha=0.2)
+     
+    plt.axvline(B_nodes[ 0], linestyle='-', c='darkblue', alpha=1.0)
+    plt.axvline(B_nodes[-1], linestyle='-', c='darkblue', alpha=1.0)
     
-    plt.xlim(xmin - 1.5*dx, xmax + 2*dx)
+    plt.axvline(xmin, linestyle=':', c='k', alpha=0.5)
+    plt.axvline(xmax, linestyle=':', c='k', alpha=0.5)
+    plt.xlabel('x (km)')
+    
     plt.legend()
     return
 
 
 def test_curl_E():
+    '''
+    Confirmed
+    '''
     NX   = 50     
     ND   = 10
     NC   = NX + 2*ND
@@ -520,99 +526,88 @@ def test_curl_E():
     xmax = 2*np.pi
     
     dx   = xmax / NX
-    k    = 2.0
+    k    = 1.0
 
-    # Physical location of nodes
-    E_nodes  = (np.arange(NC) - ND  + 0.5) * dx
-    B_nodes  = (np.arange(NC) - ND  - 0.0) * dx
-    
+    E_nodes  = (np.arange(NC    ) - ND  + 0.5) * dx
+    B_nodes  = (np.arange(NC + 1) - ND  - 0.0) * dx
+
+    # Inputs and analytic solutions
     E_input       = np.zeros((NC, 3))
-    E_input[:, 0]         =            np.cos(1.0*k*E_nodes)
-    E_input[:, 1]         =            np.cos(1.5*k*E_nodes)
-    E_input[:, 2]         =            np.cos(2.0*k*E_nodes)
+    E_input[:, 0] = np.cos(1.0*k*E_nodes)
+    E_input[:, 1] = np.cos(1.5*k*E_nodes)
+    E_input[:, 2] = np.cos(2.0*k*E_nodes)
 
-    curl_E_FD = np.zeros((NC, 3))
+    curl_E_FD = np.zeros((NC + 1, 3))
     fields.get_curl_E(E_input, curl_E_FD, DX=1)
     
-    curl_E_anal       = np.zeros((NC, 3))
-    curl_E_anal[:, 1] =  2.0 * k * np.sin(2.0*k*B_nodes)
-    curl_E_anal[:, 2] = -1.5 * k * np.sin(1.5*k*B_nodes)
-    
+    curl_E_anal       = np.zeros((NC + 1, 3))
+    curl_E_anal[:, 1] =  2.0 * k * np.sin(2.0*k*B_nodes) * dx
+    curl_E_anal[:, 2] = -1.5 * k * np.sin(1.5*k*B_nodes) * dx
     
     
     ## PLOT
     plt.figure(figsize=(15, 15))
     marker_size = None
 
-    #plt.plot(x, -deriv, linestyle=':', c='b', label='By Analytic Solution')
-    #plt.scatter(B_nodes, curl_E_anal[:, 1], marker='o', c='k', s=marker_size, label='By Node Solution')
-    plt.scatter(B_nodes, curl_E_FD[:, 1], marker='x', c='b', s=marker_size, label='By Finite Difference')
-    
-    #plt.plot(x, deriv, linestyle=':', c='r', label='Bz Analytic Solution')   
-    #plt.scatter(B_nodes, curl_E_anal[:, 2], marker='o', c='k', s=marker_size, label='Bz Node Solution')
-    plt.scatter(B_nodes, curl_E_FD[:, 2], marker='x', c='r', s=marker_size, label='Bz Finite Difference')   
+    plt.scatter(B_nodes, curl_E_anal[:, 1], marker='o', c='k', s=marker_size, label='By Node Solution')
+    plt.scatter(B_nodes, curl_E_FD[  :, 1], marker='x', c='b', s=marker_size, label='By Finite Difference')
+      
+    plt.scatter(B_nodes, curl_E_anal[:, 2], marker='o', c='k', s=marker_size, label='Bz Node Solution')
+    plt.scatter(B_nodes, curl_E_FD[  :, 2], marker='x', c='r', s=marker_size, label='Bz Finite Difference')   
     plt.title(r'Test of $\nabla \times E$')
 
     for kk in range(NC):
         plt.axvline(E_nodes[kk], linestyle='--', c='r', alpha=0.2)
         plt.axvline(B_nodes[kk], linestyle='--', c='b', alpha=0.2)
-        
-        plt.axvline(xmin, linestyle='-', c='k', alpha=0.2)
-        plt.axvline(xmax, linestyle='-', c='k', alpha=0.2)
     
-    plt.xlim(xmin - 1.5*dx, xmax + 2*dx)
+    plt.axvline(B_nodes[ 0], linestyle='-', c='darkblue', alpha=1.0)
+    plt.axvline(B_nodes[-1], linestyle='-', c='darkblue', alpha=1.0)
+    
+    plt.axvline(xmin, linestyle='-', c='k', alpha=0.2)
+    plt.axvline(xmax, linestyle='-', c='k', alpha=0.2)
+    
     plt.legend()
     return
 
 
 def test_grad_P_varying_qn():
-    NX   = 32      #const.NX
-    xmin = 0.0     #const.xmin
-    xmax = 2*np.pi #const.xmax
-    
-    dx   = xmax / NX
-    x    = np.arange(xmin, xmax, dx/100.)             # Simulation domain space 0,NX (normalized to grid)
     k    = 2.0
-    
-    # Physical location of nodes
-    E_nodes = (np.arange(NX + 3) - 0.5) * dx
-    B_nodes = (np.arange(NX + 3) - 1.0) * dx
+
+    E_nodes  = (np.arange(const.NC    ) - const.ND  + 0.5) * const.dx
+    B_nodes  = (np.arange(const.NC + 1) - const.ND  - 0.0) * const.dx
 
     # Set analytic solutions (input/output)
-    qn_input   = const.q*(np.sin(k*E_nodes) + 1)                    # Analytic input at node points (number density varying)
-    te_input   = np.ones(NX + 3)*const.Te0
+    qn_input   = np.cos(  2 * np.pi * k * E_nodes / const.xmax)  * const.q * const.ne
+    te_input   = np.ones( const.NC)*const.Te0
+    gp_diff    = np.zeros(const.NC)
+    temp       = np.zeros(const.NC + 1)
     
-    pe_anal    = const.kB * const.Te0 * k*np.cos(k*B_nodes)         # Analytic solution at nodes 
-    grad_P_anal= const.kB * const.Te0 * k*np.cos(k*x)               # Highly sampled output (derivative)
+    dne_anal   = -2 * np.pi * k * np.sin(2 * np.pi*k*E_nodes / const.xmax) * const.ne / const.xmax / const.dx
+    gp_anal    = const.kB * const.Te0 * dne_anal * const.dx          # Analytic solution at nodes 
 
     # Finite differences
-    grad_PB, grad_P     = fields.get_grad_P(qn_input, te_input, DX=dx)
-    
-    r2 = r_squared(grad_P[1:-2], pe_anal[1:-2])
-    
+    fields.get_grad_P(qn_input, te_input, gp_diff, temp)
+
     ## PLOT ##
-    plot = False
-    if plot == True:
-        plt.figure(figsize=(15, 15))
-        marker_size = None
+    plt.figure(figsize=(15, 15))
+    marker_size = None
+
+    plt.scatter(E_nodes, gp_anal*1e13, marker='o', c='k', s=marker_size, label='Node Solution')
+    plt.scatter(E_nodes, gp_diff*1e13, marker='x', c='r', s=marker_size, label='Finite Difference')
     
-        plt.plot(x, grad_P_anal, linestyle=':', c='b', label='Analytic Solution')
-        plt.scatter(B_nodes, pe_anal, marker='o', c='k', s=marker_size, label='Node Solution')
-        plt.scatter(B_nodes, grad_PB, marker='x', c='b', s=marker_size, label='Finite Difference (on B)')
-        plt.scatter(E_nodes, grad_P, marker='x', c='r', s=marker_size, label='Finite Difference (on E)')
-        
-        plt.title(r'Test of $\nabla p_e$')
+    plt.title(r'Test of $\nabla p_e$')
+
+    for kk in range(const.NC):
+        plt.axvline(E_nodes[kk], linestyle='--', c='r', alpha=0.2)
+        plt.axvline(B_nodes[kk], linestyle='--', c='b', alpha=0.2)
     
-        for kk in range(NX + 3):
-            plt.axvline(E_nodes[kk], linestyle='--', c='r', alpha=0.2)
-            plt.axvline(B_nodes[kk], linestyle='--', c='b', alpha=0.2)
-            
-            plt.axvline(xmin, linestyle='-', c='k', alpha=0.2)
-            plt.axvline(xmax, linestyle='-', c='k', alpha=0.2)
-        
-        plt.gcf().text(0.15, 0.93, '$R^2 = %.4f$' % r2)
-        plt.xlim(xmin - 1.5*dx, xmax + 2*dx)
-        plt.legend()
+    plt.axvline(B_nodes[ 0], linestyle='-', c='darkblue', alpha=1.0)
+    plt.axvline(B_nodes[-1], linestyle='-', c='darkblue', alpha=1.0)
+    
+    plt.axvline(const.xmin, linestyle='-', c='k', alpha=0.2)
+    plt.axvline(const.xmax, linestyle='-', c='k', alpha=0.2)
+    
+    plt.legend()
     return
 
 
@@ -1301,9 +1296,9 @@ if __name__ == '__main__':
     #check_position_distribution()
     #animate_moving_weight()
     #test_particle_orbit()
-    test_curl_B()
-    test_curl_E()
-    #test_grad_P_varying_qn()
+    #test_curl_B()
+    #test_curl_E()
+    test_grad_P_varying_qn()
     #test_cross_product()
     #test_cspline_interpolation()
     #test_E_convective()

simulation_codes/PREDCORR_1D_OPEN/particles_1D.py
@@ -7,8 +7,9 @@ Created on Fri Sep 22 17:23:44 2017
 import numba as nb
 import numpy as np
 
-from   simulation_parameters_1D  import ND, dx, xmax, xmin, charge, mass, kB, Tpar, Tper, drift_v, particle_boundary
+from   simulation_parameters_1D  import ND, dx, xmax, xmin, charge, mass, kB, Tpar, Tper, drift_v
 from   sources_1D                import collect_moments
+
 
 @nb.njit()
 def advance_particles_and_moments(pos, vel, Ie, W_elec, Ib, W_mag, idx, \
@@ -126,46 +127,14 @@ def position_update(pos, vel, idx, dt, Ie, W_elec):
         pos    -- Particle updated positions
         W_elec -- (0) Updated nearest E-field node value and (1-2) left/centre weights
         
-    Need to work out where I want these new particles to enter the simulation space
-    ATM they're just swapping ends, but I probably want them to go back into the middle
-    Screw it, let's just do that change.
+    Reflective boundaries to simulate the "open ends" that would have flux coming in from the ionosphere side.
     '''
     for ii in nb.prange(pos.shape[0]):
-        new_flag = 0
         pos[ii] += vel[0, ii] * dt
         
-        if pos[ii] < xmin:
-            #pos[ii] += xmax
-            new_flag = 1
-            
-        if pos[ii] > xmax:
-            #pos[ii] -= xmax
-            new_flag = 2
+        if pos[ii] < xmin or pos[ii] > xmax:
+            vel[0, ii] *= -1.                   # Reflect velocity
+            pos[ii]    += vel[0, ii] * dt       # Get particle back in simulation space
 
-        if new_flag > 0:
-            
-            if particle_boundary == 'renew':
-                # Re-initialize temperature. "New" particle. 
-                # Should be able to disable this functionality by replacing
-                # if statement with "False"
-                pos[ii]    = xmax / 2.          # This might give me crazy density errors at some point
-                
-                sp         = idx[ii]
-                vel[0, ii] = np.random.normal(0, np.sqrt(kB *  Tpar[sp] /  mass[sp]) +  drift_v[sp])
-                vel[1, ii] = np.random.normal(0, np.sqrt(kB *  Tper[sp] /  mass[sp]))
-                vel[2, ii] = np.random.normal(0, np.sqrt(kB *  Tper[sp] /  mass[sp]))
-            elif particle_boundary == 'reflect':
-                # Probably the least problematic? Except could cause weird things... maybe?
-                vel[0, ii] *= -1.                   # Reflect velocity
-                pos[ii]    += vel[0, ii] * dt       # Get particle back in simulation space
-            else:
-                # Terribly inefficient. Maybe I can use the idx flag with a -1 to indicate particles
-                # that have been "turned off"? Their species probably doesn't matter too much after
-                # they've left the simulation space. Also, might this screw with node calc. equations?
-                pos[ii]    = - xmax
-                vel[0, ii] = 0.
-                vel[1, ii] = 0.
-                vel[2, ii] = 0.
-            
     assign_weighting_TSC(pos, Ie, W_elec)
     return

simulation_codes/PREDCORR_1D_OPEN/save_routines.py
@@ -13,7 +13,7 @@ from shutil import rmtree
 import simulation_parameters_1D as const
 from   simulation_parameters_1D import drive, save_path, NX, ND, NC, r_damp, ne, density, save_particles, save_fields
 from   simulation_parameters_1D import idx_bounds, Nj, species_lbl, temp_type, dist_type, mass, charge,\
-                                       drift_v, Tpar, Tper, temp_color, nsp_ppc, particle_boundary, Bc
+                                       drift_v, Tpar, Tper, temp_color, nsp_ppc, Bc
 
 
 def manage_directories():
@@ -78,7 +78,7 @@ def store_run_parameters(dt, part_save_iter, field_save_iter):
                    ('method_type', 'PREDCORR_HM'),
                    ('particle_shape', 'TSC'),
                    ('boundary_type', 'damped'),
-                   ('particle_boundary', particle_boundary),
+                   ('particle_boundary', 'reflective'),
                    ])
 
     with open(d_path + 'simulation_parameters.pckl', 'wb') as f:
@@ -104,8 +104,7 @@ def store_run_parameters(dt, part_save_iter, field_save_iter):
     return
 
 
-def save_field_data(dt, field_save_iter, qq, Ji, E, B, Ve, Te, dns):
-    sim_time = np.array([qq*dt])    # Timestamp: Useful for debugging
+def save_field_data(sim_time, dt, field_save_iter, qq, Ji, E, B, Ve, Te, dns):
     d_path   = '%s/%s/run_%d/data/fields/' % (drive, save_path, const.run_num)
     r        = qq / field_save_iter
 
@@ -116,8 +115,7 @@ def save_field_data(dt, field_save_iter, qq, Ji, E, B, Ve, Te, dns):
     print('Field data saved')
     
     
-def save_particle_data(dt, part_save_iter, qq, pos, vel):
-    sim_time = np.array([qq*dt])    # Timestamp: Useful for debugging
+def save_particle_data(sim_time, dt, part_save_iter, qq, pos, vel):
     d_path   = '%s/%s/run_%d/data/particles/' % (drive, save_path, const.run_num)
     r        = qq / part_save_iter
 

simulation_codes/PREDCORR_1D_OPEN/simulation_parameters_1D.py
@@ -13,9 +13,9 @@ run_description = '''Testing to see if I can get this open boundary thing to wor
 ### RUN PARAMETERS ###
 drive           = 'F:'                          # Drive letter or path for portable HDD e.g. 'E:/' or '/media/yoshi/UNI_HD/'
 save_path       = 'runs//open_boundary_test'    # Series save dir   : Folder containing all runs of a series
-run_num         = 1                             # Series run number : For multiple runs (e.g. parameter studies) with same overall structure (i.e. test series)
-save_particles  = 1                             # Save data flag    : For later analysis
-save_fields     = 1                             # Save plot flag    : To ensure hybrid is solving correctly during run
+run_num         = 2                             # Series run number : For multiple runs (e.g. parameter studies) with same overall structure (i.e. test series)
+save_particles  = 0                             # Save data flag    : For later analysis
+save_fields     = 0                             # Save plot flag    : To ensure hybrid is solving correctly during run
 seed            = 3216587                       # RNG Seed          : Set to enable consistent results for parameter studies
 cpu_affin       = [(2*run_num)%8, (2*run_num + 1)%8]      # Set CPU affinity for run. Must be list. Auto-assign: None.
 
@@ -32,12 +32,12 @@ RE  = 6.371e6                               # Earth radius in metres
 
 
 ### SIMULATION PARAMETERS ###
-NX       = 512                              # Number of cells - doesn't include ghost cells
-ND       = 128                              # Damping region length: Multiple of NX (on each side of simulation domain)
+NX       = 128                              # Number of cells - doesn't include ghost cells
+ND       = 32                               # Damping region length: Multiple of NX (on each side of simulation domain)
 max_rev  = 100                              # Simulation runtime, in multiples of the ion gyroperiod (in seconds)
 r_damp   = 0.0129                           # Damping strength
 
-nsp_ppc  = 2000                             # Number of particles per cell, per species - i.e. each species has equal representation (or code this to be an array later?)
+nsp_ppc  = 200                              # Number of particles per cell, per species - i.e. each species has equal representation (or code this to be an array later?)
 dxm      = 1.0                              # Number of c/wpi per dx (Ion inertial length: anything less than 1 isn't "resolvable" by hybrid code, anything too much more than 1 does funky things to the waveform)
 
 ie       = 1                                # Adiabatic electrons. 0: off (constant), 1: on.
@@ -60,7 +60,7 @@ mass       = np.asarray([1., 1.])    			            # Species ion mass (proton m
 charge     = np.asarray([1., 1.])    			            # Species ion charge (elementary charge units)
 drift_v    = np.asarray([0., 0.])                           # Species parallel bulk velocity (alfven velocity units)
 density    = np.asarray([180. , 20.]) * 1e6                 # Species density in /cc (cast to /m3)
-E_per      = np.array([5.0, 60000.0])
+E_per      = np.array([5.0, 30000.0])
 anisotropy = np.array([0.0, 4.0])
 
 smooth_sources = 0                                          # Flag for source smoothing: Gaussian
@@ -70,7 +70,6 @@ E_e            = 200.0                                      # Electron energy (e
 account_for_dispersion = False                              # Flag (True/False) whether or not to reduce timestep to prevent dispersion getting too high
 dispersion_allowance   = 1.                                 # Multiple of how much past frac*wD^-1 is allowed: Used to stop dispersion from slowing down sim too much  
 adaptive_timestep      = True                               # Flag (True/False) for adaptive timestep based on particle and field parameters
-particle_boundary      = 'reflect'                          # Renew (at cell center), Reflect (off boundary), or None (Or anything else, particles leave simulation space)
 
 HM_amplitude   = 0e-9                                       # Driven wave amplitude in T
 HM_frequency   = 0.02                                       # Driven wave in Hz