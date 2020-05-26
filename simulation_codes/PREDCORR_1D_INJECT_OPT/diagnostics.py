Fixed MLAT and B0 calculation to ACTUALLY reflect dipole
Also saving before I fuck with particles.

simulation_codes/PREDCORR_1D_INJECT_OPT/diagnostics.py
@@ -95,6 +95,71 @@ def check_cell_velocity_distribution(pos, vel, node_number=const.NX // 2, j=0): 
     plt.show()
     return
 
+
+def check_cell_velocity_distribution_2D(pos, vel, node_number=const.NX // 2, jj=0, show_cone=False, save=False): #
+    '''
+    Checks the velocity distribution of a particle species within a specified cell
+    
+    Starts at "cell 0" - i.e. only counts real cells. 
+    Damping cell offset is handled by function
+    '''
+    if node_number is None:
+        node_number = np.arange(const.NX)
+        
+    # Account for damping nodes. Node_number should be "real" node count.
+    for node in node_number:
+        print('Collecting particle info in cell {}'.format(node))
+        node += const.ND
+        x_node = const.E_nodes[node]
+        f      = np.zeros((1, 3))
+        
+        count = 0
+        for ii in np.arange(const.idx_start[jj], const.idx_end[jj]):
+            if (abs(pos[0, ii] - x_node) <= 0.5*const.dx):
+                f = np.append(f, [vel[0:3, ii]], axis=0)
+                count += 1
+    
+        plt.ioff()
+        print('{} particles counted for diagnostic'.format(count))
+        fig, ax1 = plt.subplots(figsize=(15,10))
+        fig.suptitle('Particle velocity distribution of species {} in cell {}'.format(jj, node))
+        fig.patch.set_facecolor('w')
+    
+        V_PERP = np.sign(f[:, 2]) * np.sqrt(f[:, 1] ** 2 + f[:, 2] ** 2) / const.va
+        V_PARA = f[:, 0] / const.va
+    
+        ax1.scatter(V_PERP, V_PARA, s=1, c=const.temp_color[jj])
+    
+        ax1.set_ylabel('$v_\parallel (/v_A)$')
+        ax1.set_xlabel('$v_\perp (/v_A)$')
+        ax1.axis('equal')
+        
+        # Plot loss cone lines 
+        B_at_nodes     = fields.eval_B0x(np.array([x_node + const.dx, x_node - const.dx]))
+        end_loss_cones = np.arcsin(np.sqrt(B_at_nodes/const.B_loss)).max()                    # Calculate max loss cone
+        
+        yst, yend = ax1.get_ylim()                  # Get V_PARA lims
+        yarr      = np.linspace(yst, yend, 100)     # V_PARA values on axis
+        xarr      = yarr * np.tan(end_loss_cones)   # Calculate V_PERP values
+        ax1.plot(xarr,  yarr, c='k', label='Loss Cone: {:.1f} deg'.format(end_loss_cones * 180. / np.pi))
+        ax1.plot(xarr, -yarr, c='k')
+        ax1.legend()
+        ax1.set_xlim(yst, yend)
+        ax1.set_ylim(yst, yend)
+        
+        if save == True:
+            save_path = const.drive + '//' + const.save_path + '//' + 'run_{}//'.format(const.run)
+            
+            if os.path.exists(save_path) == False:
+                os.makedirs(save_path)
+                
+            fig.savefig(save_path + 'LCD_cell_{:04}.png'.format(node))
+            plt.close('all')
+        else:
+            plt.show()
+    return
+
+
 def check_position_distribution(pos):
     '''Checks the spatial distribution of a particle species j within the spatial domain
     
@@ -1099,18 +1164,24 @@ def test_interp_cross_manual():
 
 
 
-def plot_dipole_field_line(length=True):
+def plot_dipole_field_line(length=True, get_from_sim=True):
     '''
     Plots field lines with basic L = r*sin^2(theta) relation. Can plot
     multiple for all in Ls. Can also calculate arclengths from lat_st/lat_min
     and print as the title (can be changed if you want)
     '''
-    Ls         = [4.3]
+    
     dtheta     = 0.1
     theta      = np.arange(0, 180. + dtheta, dtheta) * np.pi / 180
         
-    lat_st = 80  
-    lat_en = 100
+    if get_from_sim == False:
+        Ls     = [4.3]
+        lat_st = 80  
+        lat_en = 100
+    else:
+        Ls     = [const.L]
+        lat_st = 90 - const.theta_xmax * 180./np.pi
+        lat_en = 90 + const.theta_xmax * 180./np.pi
     
     plt.figure()
     plt.gcf().gca().add_artist(plt.Circle((0,0), 1.0, color='k'))
@@ -1134,7 +1205,7 @@ def plot_dipole_field_line(length=True):
         length = 0
         for ii in range(idx_start, idx_end):
             length += r[ii] * dtheta * np.pi / 180
-        plt.title('Arclength from {} deg to {} deg at L = {} : {:>5.2f} R_E'.format(lat_st, lat_en, L, length))
+        plt.title('Arclength MLAT {} to {} deg at L = {} : {:>5.2f} R_E'.format(90 - lat_st, 90 + lat_en, L, length))
     return
 
 
@@ -2443,7 +2514,7 @@ if __name__ == '__main__':
     #test_curl_B()
     #test_curl_E()
     #test_grad_P()
-    test_grad_P_with_init_loading()
+    #test_grad_P_with_init_loading()
     #test_density_and_velocity_deposition()
     #check_density_deposition()
     #visualize_inhomogenous_B()
@@ -2473,7 +2544,7 @@ if __name__ == '__main__':
     #smart_plot_2D_planes()
     #smart_plot_3D()
     #check_directions()
-
+    plot_dipole_field_line(length=True)
 # =============================================================================
 #     init_pos, init_vel, time, pos_history, vel_history, mag_history,\
 #         DT, max_t, POS_gphase, VEL_gphase = do_particle_run(max_rev=1, v_mag=10.0, pitch=41.0, dt_mult=1.0)

simulation_codes/PREDCORR_1D_INJECT_OPT/init_1D.py
@@ -9,28 +9,41 @@ import numba as nb
 import numpy as np
 import simulation_parameters_1D as const
 import save_routines as save
-#import diagnostics as diag
+import diagnostics as diag
 
 import particles_1D as particles
 import fields_1D    as fields
 
-from simulation_parameters_1D import dx, NX, ND, NC, N, kB, Nj, nsp_ppc, va, B_xmax,   \
+from simulation_parameters_1D import dx, NX, ND, NC, N, kB, Nj, nsp_ppc, va, B_loss,   \
                                      idx_start, idx_end, seed, Tpar, Tper, mass, drift_v,  \
                                      qm_ratios, freq_res, rc_hwidth, temp_type, Te0_scalar, ne, q
 
 
+@nb.njit()
 def calc_losses(v_para, v_perp, B0x, st=0):
     '''
     For arrays of parallel and perpendicular velocities, finds the number and 
     indices of particles outside the loss cone.
-    
-    Calculation of in_loss_cone not compatible with njit(). Recode later if you want.
     '''
     alpha        = np.arctan(v_perp / v_para)                   # Calculate particle PA's
-    loss_cone    = np.arcsin(np.sqrt(B0x / B_xmax))             # Loss cone per particle (based on B0 at particle)
-    in_loss_cone = (abs(alpha) < loss_cone)                     # Determine if particle in loss cone
+    loss_cone    = np.arcsin(np.sqrt(B0x / B_loss))             # Loss cone per particle (based on B0 at particle)
+    #pdb.set_trace()
+    
+    in_loss_cone = np.zeros(v_para.shape[0], dtype=nb.int32)
+    for ii in range(v_para.shape[0]):
+        if np.abs(alpha[ii]) < loss_cone[ii]:                   # Determine if particle in loss cone
+            in_loss_cone[ii] = 1
+    
     N_loss       = in_loss_cone.sum()                           # Count number that are
-    loss_idx     = np.where(in_loss_cone == True)[0]            # Find their indices
+    
+    # Collect the indices of those in the loss cone
+    loss_idx     = np.zeros(N_loss, dtype=nb.int32)
+    lc           = 0
+    for ii in range(v_para.shape[0]):
+        if in_loss_cone[ii] == True:
+            loss_idx[lc] = ii
+            lc          += 1
+        
     loss_idx    += st                                           # Offset indices to account for position in master array
     return N_loss, loss_idx
 
@@ -69,7 +82,7 @@ def LCD_by_rejection(pos, vel, sf_par, sf_per, st, en, jj):
     Takes in a Maxwellian or pseudo-maxwellian distribution. Outputs the number
     and indexes of any particle inside the loss cone
     '''
-    B0x = fields.eval_B0x(pos[0, st: en])
+    B0x    = fields.eval_B0x(pos[0, st: en])
     N_loss = const.N_species[jj]
                 
     while N_loss > 0:
@@ -91,6 +104,7 @@ def LCD_by_rejection(pos, vel, sf_par, sf_per, st, en, jj):
             vel[1, loss_idx] = np.random.normal(0., sf_per, N_loss)
             vel[2, loss_idx] = np.random.normal(0., sf_per, N_loss)
     return
+
 
 def uniform_gaussian_distribution_quiet():
     '''Creates an N-sampled normal distribution across all particle species within each simulation cell
@@ -476,17 +490,21 @@ if __name__ == '__main__':
     
     node_number = 0
     
-    for jj in range(const.Nj):
-        if True:
-            # Loss cone diagram
-            fig1, ax1 = plt.subplots()
-            
-            ax1.scatter(V_PERP[idx_start[jj]: idx_end[jj]], V_PARA[idx_start[jj]: idx_end[jj]], s=1, c=const.temp_color[jj])
-
-            ax1.set_title('Loss Cone Distribution :: {}'.format(const.species_lbl[jj]))
-            ax1.set_ylabel('$v_\parallel (/v_A)$')
-            ax1.set_xlabel('$v_\perp (/v_A)$')
-            ax1.axis('equal')
+    diag.check_cell_velocity_distribution_2D(POS, VEL, node_number=None, jj=1, save=True)
+    
+# =============================================================================
+#     for jj in range(const.Nj):
+#         if True:
+#             # Loss cone diagram
+#             fig1, ax1 = plt.subplots()
+#             
+#             ax1.scatter(V_PERP[idx_start[jj]: idx_end[jj]], V_PARA[idx_start[jj]: idx_end[jj]], s=1, c=const.temp_color[jj])
+# 
+#             ax1.set_title('Loss Cone Distribution :: {}'.format(const.species_lbl[jj]))
+#             ax1.set_ylabel('$v_\parallel (/v_A)$')
+#             ax1.set_xlabel('$v_\perp (/v_A)$')
+#             ax1.axis('equal')
+# =============================================================================
      
 # =============================================================================
 #             ax2.scatter(POS[1, idx_start[jj]: idx_end[jj]], POS[2, idx_start[jj]: idx_end[jj]], s=1, c=const.temp_color[jj])
@@ -501,8 +519,6 @@ if __name__ == '__main__':
 #             ax3.set_xlabel('$v_z (m/s)$')
 #             ax3.axis('equal')
 # =============================================================================
-            
-            plt.show()
             
 # =============================================================================
 #         if False:

simulation_codes/PREDCORR_1D_INJECT_OPT/simulation_parameters_1D.py
@@ -31,14 +31,15 @@ particle_boundary = 1                             # 0: Absorb, 1: Flux, 2: Perio
 convert_hot       = False                         # Convert hot particles to cold when reinitialized
 
 ### SIMULATION PARAMETERS ###
-NX        = 128                             # Number of cells - doesn't include ghost cells
+NX        = 500                             # Number of cells - doesn't include ghost cells
 ND        = 64                              # Damping region length: Multiple of NX (on each side of simulation domain)
 max_rev   = 50                              # Simulation runtime, in multiples of the ion gyroperiod (in seconds)
-dxm       = 1.0                             # Number of c/wpi per dx (Ion inertial length: anything less than 1 isn't "resolvable" by hybrid code, anything too much more than 1 does funky things to the waveform)
+dxm       = 10.0                            # Number of c/wpi per dx (Ion inertial length: anything less than 1 isn't "resolvable" by hybrid code, anything too much more than 1 does funky things to the waveform)
 L         = 5.35                            # Field line L shell
+r_A       = 150e3                           # Ionospheric anchor point (loss zone/max mirror point)
 
 ie        = 1                               # Adiabatic electrons. 0: off (constant), 1: on.
-B_eq      = 200e-9                          # Initial magnetic field at equator: None for L-determined value (in T)
+B_eq      = None                            # Initial magnetic field at equator: None for L-determined value (in T)
 rc_hwidth = 0                               # Ring current half-width in number of cells (2*hwidth gives total cells with RC) 
   
 orbit_res = 0.02                            # Orbit resolution
@@ -52,7 +53,7 @@ species_lbl= [r'$H^+$ cold', r'$H^+$ warm']                 # Species name/label
 temp_color = ['blue', 'red']
 temp_type  = np.array([0, 1])             	                # Particle temperature type  : Cold (0) or Hot (1) : Hot particles get the LCD, cold are maxwellians.
 dist_type  = np.array([0, 0])                               # Particle distribution type : Uniform (0) or sinusoidal/other (1) : Used for plotting (normalization)
-nsp_ppc    = np.array([200, 500])                           # Number of particles per cell, per species
+nsp_ppc    = np.array([100, 10000])                         # Number of particles per cell, per species
 
 mass       = np.array([1., 1.])    			                # Species ion mass (proton mass units)
 charge     = np.array([1., 1.])    			                # Species ion charge (elementary charge units)
@@ -89,12 +90,12 @@ mu0    = (4e-7) * np.pi                     # Magnetic Permeability of Free Spac
 RE     = 6.371e6                            # Earth radius in metres
 B_surf = 3.12e-5                            # Magnetic field strength at Earth surface
 
-NC         = NX + 2*ND
-ne         = density.sum()
-E_par      = E_per / (anisotropy + 1)
+NC         = NX + 2*ND                      # Total number of cells
+ne         = density.sum()                  # Electron number density
+E_par      = E_per / (anisotropy + 1)       # Parallel species energy
 
 if B_eq is None:
-    B_eq      = (B_surf / (L ** 3))                      # Magnetic field at equator, based on L value
+    B_eq      = (B_surf / (L ** 3))         # Magnetic field at equator, based on L value
     
 if beta_par is None:
     Te0_scalar = E_e   * 11603.
@@ -145,11 +146,20 @@ idx_end    = np.asarray([np.sum(N_species[0:ii + 1])     for ii in range(0, Nj)]
 B_nodes  = (np.arange(NC + 1) - NC // 2)       * dx      # B grid points position in space
 E_nodes  = (np.arange(NC)     - NC // 2 + 0.5) * dx      # E grid points position in space
 
-theta_xmax  = xmax/(L*RE)                                # Latitudinal extent of simulation , based on xmax
-r_xmax      = L * np.sin(np.pi / 2 - theta_xmax) ** 2    # Calculate radial distance of boundary in dipole and get field intensity
-cos_bit     = np.sqrt(3*np.cos(theta_xmax)**2 + 1)       # Intermediate variable (angular scale factor)
-B_xmax      = (B_surf / (r_xmax ** 3)) * cos_bit         # Magnetic field intensity at boundary
-a           = (B_xmax / B_eq - 1) / xmax ** 2            # Parabolic scale factor: Fitted to B_eq, B_xmax
+print('Fitting parabola to dipole...')
+dlam   = 1e-6                                            # Latitude increment in radians
+fx_len = 0.0; ii = 1                                     # Arclength/increment counters
+while fx_len < xmax:
+    lam_i   = dlam * ii                                                             # Current latitude
+    d_len   = L * RE * np.cos(lam_i) * np.sqrt(4 - 3*np.cos(lam_i) ** 2) * dlam     # Length increment
+    fx_len += d_len                                                                 # Accrue arclength
+    ii     += 1                                                                     # Increment counter
+
+theta_xmax  = lam_i                                                                 # Latitude of simulation boundary
+r_xmax      = L * RE * np.cos(theta_xmax) ** 2                                      # Radial distance of simulation boundary
+B_xmax      = B_eq*np.sqrt(4 - 3*np.cos(theta_xmax)**2)/np.cos(theta_xmax)**6       # Magnetic field intensity at boundary
+a           = (B_xmax / B_eq - 1) / xmax ** 2                                       # Parabolic scale factor: Fitted to B_eq, B_xmax
+lambda_L    = np.arccos(np.sqrt(1.0 / L))                                           # Lattitude of Earth's surface at this L
 
 if homogenous == True:
     a      = 0
@@ -166,7 +176,11 @@ gyfreq_eq  = q*B_eq  / mp                                # Proton Gyrofrequency 
 k_max      = np.pi / dx                                  # Maximum permissible wavenumber in system (SI???)
 qm_ratios  = np.divide(charge, mass)                     # q/m ratio for each species
 
-loss_cone  = np.arcsin(np.sqrt(B_eq / B_xmax))*180 / np.pi # Loss cone in degrees
+lat_A      = np.arccos(np.sqrt((RE + r_A)/(RE*L)))       # Anchor latitude in radians
+B_loss     = B_eq * np.sqrt(4 - 3*np.cos(lat_A) ** 2)\
+           / (np.cos(lat_A) ** 6)                        # Magnetic field at anchor point
+
+loss_cone  = np.arcsin(np.sqrt(B_eq / B_loss))*180 / np.pi # Equatorial loss cone in degrees
 
 
 #%%### INPUT TESTS AND CHECKS
@@ -206,8 +220,9 @@ if supress_text == False:
     print('Density            : {:5.2f}cc'.format(ne / 1e6))
     print('Equatorial B-field : {:5.2f}nT'.format(B_eq*1e9))
     print('Maximum    B-field : {:5.2f}nT'.format(B_xmax*1e9))
-    print('Loss cone          : {:<5.2f} degrees  '.format(loss_cone))
-    print('Maximum MLAT (+/-) : {:<5.2f} degrees\n'.format(theta_xmax * 180. / np.pi))
+    print('Equat. Loss cone   : {:<5.2f} degrees  '.format(loss_cone))
+    print('Maximum MLAT (+/-) : {:<5.2f} degrees  '.format(theta_xmax * 180. / np.pi))
+    print('Iono.   MLAT (+/-) : {:<5.2f} degrees\n'.format(lambda_L * 180. / np.pi))
     
     print('Equat. Gyroperiod: : {}s'.format(round(2. * np.pi / gyfreq, 3)))
     print('Inverse rad gyfreq : {}s'.format(round(1 / gyfreq, 3)))
@@ -238,6 +253,13 @@ if abs(simulated_density_per_cell - real_density_per_cell) / real_density_per_ce
     print('--------------------------------------------------------------------------------')
     print('')
     print('ABORTING...')
+    sys.exit()
+
+if theta_xmax > lambda_L:
+    print('--------------------------------------------------')
+    print('WARNING : SIMULATION DOMAIN LONGER THAN FIELD LINE')
+    print('DO SOMETHING ABOUT IT')
+    print('--------------------------------------------------')
     sys.exit()
 
 if particle_boundary != 0: