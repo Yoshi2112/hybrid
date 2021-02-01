d7b8117c57cef7da7c3ccb855886dc12c61a3ba6
Yoshi2112 <joshua.s.williams@uon.edu.au>
2021-02-01 22:20:19 +1100

    new batch run for Shoji test

--- linear_theory/Non_linear_stuff/geopack_test.py
+++ linear_theory/Non_linear_stuff/geopack_test.py
@@ -4,9 +4,10 @@ Created on Sun Jan 31 11:17:17 2021
 
 @author: Yoshi
 """
-#import matplotlib.pyplot as plt
-#import numpy as np
-import PyGeopack as gp
+import pdb
+import matplotlib.pyplot as plt
+import numpy as np
+import geopack as gp
 
 # Want to use geopack to give me a trace of the magnetic field alone a field
 # line, and somehow convert (x, y, z) in GSM (?) to an s-value
@@ -15,16 +16,83 @@ import PyGeopack as gp
 # standard intervals along the line, then retrieve the fields at those points
 # using whatever? That seems like the easy part (if each point has MLAT, r)
 
-gp.UpdateParameters(SkipWParameters=True)
+def read_QIN_file(filepath):
+    '''
+    Hard-coded because I'm too lazy to learn JSON
+    '''
+    # Could potentially be hardcoded with n_lines = 1440 but this is safer
+    print('Reading', filepath)
+    n_lines = 0
+    with open(filepath) as f:
+        for line in f:
+            if line[0] != '#':
+                n_lines += 1
+    
+    # Initialize arrays
+    year, month, day, hour, minute, second, ByIMF, BzIMF, Vsw, den_P, Pdyn,  \
+    ByIMF_status, BzIMF_status, Vsw_status, den_P_status, Pdyn_status, \
+    Kp, akp3, Dst, = [np.zeros(n_lines) for _ in range(19)]
+    
+    epoch    = np.zeros((n_lines), dtype=str)
+    G        = np.zeros((n_lines, 3))
+    G_status = np.zeros((n_lines, 3))
+    Bz       = np.zeros((n_lines, 6))
+    W        = np.zeros((n_lines, 6))
+    W_status = np.zeros((n_lines, 6))
+    
+    # Pack in dict? Do later
+    with open(filepath) as f:
+        
+        ii = 0
+        for line in f:
+            if line[0] != '#':
+                A = line.split()
 
-L_value = 6          # GSM: (L, 0, 0) would be the equatorial point
+                epoch[ii]        = A[0]
+                year[ii]         = int(A[1])
+                month[ii]        = int(A[2])
+                day[ii]          = int(A[3])
+                hour[ii]         = int(A[4])
+                minute[ii]       = int(A[5])
+                second[ii]       = int(A[6])
+                ByIMF[ii]        = float(A[7])
+                BzIMF[ii]        = float(A[8])
+                Vsw[ii]          = float(A[9])
+                den_P[ii]        = float(A[10])
+                Pdyn[ii]         = float(A[11])
+                G[ii, 0] = float(A[12])
+                G[ii, 1] = float(A[13])
+                G[ii, 2] = float(A[14])
+                ByIMF_status[ii] = float(A[15])
+                BzIMF_status[ii] = float(A[16])
+                Vsw_status[ii]   = float(A[17])
+                den_P_status[ii] = float(A[18])
+                Pdyn_status[ii]  = float(A[19])
+                G_status[ii, 0]     = float(A[20])
+                G_status[ii, 1]     = float(A[21])
+                G_status[ii, 2]     = float(A[22])
+                Kp[ii]           = float(A[23])
+                akp3[ii]         = float(A[24])
+                Dst[ii]          = float(A[25])
+                Bz[ii, 0] = float(A[26]); Bz[ii, 1] = float(A[27]); Bz[ii, 2] = float(A[28])
+                Bz[ii, 3] = float(A[29]); Bz[ii, 4] = float(A[30]); Bz[ii, 5] = float(A[31])
+                W[ii, 0]  = float(A[32]); W[ii, 1]  = float(A[33]); W[ii, 2]  = float(A[34])
+                W[ii, 3]  = float(A[35]); W[ii, 4]  = float(A[36]); W[ii, 5]  = float(A[37])
+                W_status[ii, 0] = float(A[38]); W_status[ii, 1] = float(A[39])
+                W_status[ii, 2] = float(A[40]); W_status[ii, 3] = float(A[41])
+                W_status[ii, 4] = float(A[42]); W_status[ii, 5] = float(A[43])
+                
+                ii += 1        
+    return
+
+
+if __name__ == '__main__':
+    FPATH = 'G://DATA//QIN_DENTON//2020//QinDenton_20200101_1min.txt'
+    read_QIN_file(FPATH)
 
 # =============================================================================
-# T = gp.TraceField(L_value, 0.0, 0.0, 20210131, 21.0, Model='T96',CoordIn='GSM',CoordOut='GSM',
-# 		alt=100.0,MaxLen=1000,DSMax=1.0,FlattenSingleTraces=True,Verbose=True)
-# =============================================================================
-
-# =============================================================================
+# L_value = 6          # GSM: (L, 0, 0) would be the equatorial point
+# 
 # xf, yf, zf, xn, yn, zn=gp.geopack.trace(L_value, 0.0, 0.0, -1)
 # xf, yf, zf, xs, ys, zs=gp.geopack.trace(L_value, 0.0, 0.0, 1)
 # 
--- linear_theory/Non_linear_stuff/phase_space_plots_SI.py
+++ linear_theory/Non_linear_stuff/phase_space_plots_SI.py
@@ -5,7 +5,7 @@ Created on Thu Jan 14 21:56:57 2021
 @author: Yoshi
 """
 ### IMPORTS
-import warnings, sys, pdb
+import warnings, sys, pdb, os
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -35,20 +35,25 @@ Test:
     
 For a given frequency, there is unique k, and velocities under the cold approximation
 '''
-def new_trace_fieldline(L):
+def new_trace_fieldline(L, Npts=1e5):
     '''
     Traces field line position and B-intensity for given L. 
     Traces half, flips and copies the values, and then
     reverses sign of s since the integration is done from the southern hemisphere
     but we normally think of traces from the northern.
-    
+        
     Validated: Ionospheric B0 goes to surface B0 for rA = 0.
     '''
     BE     = 0.31*1e5   # Equatorial field strength in nT
     r_A    = 120e3 
     RE     = 6.371e6
 
-    Np       = int(1e5+1)
+    # Force Np odd:
+    if Npts%2 == 0:
+        Np = int(Npts+1)
+    else:
+        Np = int(Npts)
+        
     iono_lat = np.arccos(np.sqrt((RE + r_A)/(RE*L))) 
     mlat     = np.linspace(-iono_lat, iono_lat, Np, endpoint=True)
     dlat     = mlat[1] - mlat[0]
@@ -68,13 +73,7 @@ def new_trace_fieldline(L):
     # Flip around equator for 1st half
     s[ :mid_idx] = -1.0*np.flip( s[mid_idx + 1:])
     Bs[:mid_idx] =      np.flip(Bs[mid_idx + 1:])
-        
-    # Check surface field measurement
-    #BL = BE * np.sqrt(4 - 3/L)
-    
-    # Reverse s sign, convert to T and degrees respectively
     return -s, Bs*1e-9, -mlat*180./np.pi
-
 
 def get_B_gradient(B, s):
     '''
@@ -92,7 +91,6 @@ def get_B_gradient(B, s):
     grad[0]  = (-3 * B[0] + 4 * B[1] - B[2]) / (s[2] - s[0])
     grad[-1] = (B[-1] - 4*B[-2] + 3*B[-3]) / (s[-3] - s[-1])
     return grad
-
 
 def create_species_array(B0, name, mass, charge, density, tper, ani):
     '''
@@ -141,40 +139,20 @@ def create_species_array(B0, name, mass, charge, density, tper, ani):
     PlasParams['B0']       = B0                                        # Magnetic field value (T)
     return Species, PlasParams
 
-
-def define_omura2010_parameters(include_energetic=False):
+def define_omura2010_parameters(B0):
     '''
     Ambient plasma parameters from Omura et al. (2010) to recreate plots
     '''
-    # Parameters in SI units (Note: Added the hot bit here. Is it going to break anything?) nh = 7.2
-    pcyc    = 3.7 # Hz
-    B0      = 2 * np.pi * mp * pcyc / qp
-    
-    if include_energetic == True:
-        Th_para  = (mp * (6e5)**2 / kB) / 11603.
-        Th_perp  = (mp * (8e5)**2 / kB) / 11603.
-        Ah       = Th_perp / Th_para - 1
-        #apar_h   = np.sqrt(2.0 * qp * Th_para  / mp)
-    
-        name    = np.array(['H'    , 'He'  , 'O'  , 'Hot H'])
-        mass    = np.array([1.0    , 4.0   , 16.0 , 1.0    ]) * mp
-        charge  = np.array([1.0    , 1.0   , 1.0  , 1.0    ]) * qp
-        density = np.array([136.8  , 17.0  , 17.0 , 7.2    ]) * 1e6
-        ani     = np.array([0.0    , 0.0   , 0.0  , Ah])
-        tpar    = np.array([0.0    , 0.0   , 0.0  , Th_para])
-        tper    = (ani + 1) * tpar
-    else:
-        name    = np.array(['H'    , 'He'  , 'O' ])
-        mass    = np.array([1.0    , 4.0   , 16.0]) * mp
-        charge  = np.array([1.0    , 1.0   , 1.0 ]) * qp
-        density = np.array([144.0  , 17.0  , 17.0]) * 1e6
-        ani     = np.array([0.0    , 0.0   , 0.0 ])
-        tpar    = np.array([0.0    , 0.0   , 0.0 ])
-        tper    = (ani + 1) * tpar
+    name    = np.array(['H'    , 'He'  , 'O' ])
+    mass    = np.array([1.0    , 4.0   , 16.0]) * mp
+    charge  = np.array([1.0    , 1.0   , 1.0 ]) * qp
+    density = np.array([144.0  , 17.0  , 17.0]) * 1e6
+    ani     = np.array([0.0    , 0.0   , 0.0 ])
+    tpar    = np.array([0.0    , 0.0   , 0.0 ])
+    tper    = (ani + 1) * tpar
     
     Species, PP = create_species_array(B0, name, mass, charge, density, tper, ani)
     return Species, PP
-
 
 def get_k_cold(w, Species):
     with warnings.catch_warnings():
@@ -220,8 +198,7 @@ def get_velocities(w, k, Species, PP):
     Vp = get_phase_velocity(w, k, Species)
     return Vg, Vp, Vr
 
-
-def plot_SI_phase_space():
+def get_S_dipole(L_shell):
     '''
     Define a Bw and w in a specified plasma regime.
     
@@ -230,74 +207,130 @@ def plot_SI_phase_space():
     Perhaps try using parabolic field. Calculate 'a' factor by
     tracing parabola up to MLAT=10 degrees or something, or just steal some
     standard value from the model.
-    '''
-    Species, PP = define_omura2010_parameters()
-
-    # Wave parameters
-    f    = 1.5                         # Hz
-    w    = 2*np.pi*f                   # rad/s
-    k    = get_k_cold(w, Species)      # /m ??
-    Bw   = 0.5e-9                      # Initial EMIC (triggering?) amplitude
-    omw  = qp * Bw / mp                # 'Gyrofrequency' effect of wave field
-    dwdt = 0*2 * np.pi * (1.2/48)        # Frequency sweep rate (rad/s2) 1.2Hz in 48sec
-    pcyc = PP['pcyc_rad']
-
-    # Hot proton parameters
-    nh       = 7.2e6       # /m3
-    vth_para = 6e5         # m/s
-    vth_perp = 8e5         # m/s
-    Q_value  = 0.5         # Depth of proton hole
     
-    # Field parameters (L = 4.27)
-    L_shell     = 4.27
+    Stanard hc range around 320km with only low amplitude triggering wave
+    3700km by the time the non-linear growth has maximized and the wave has grown
+    
+    START EASY :: Just calculate S along the field, calculate phase space for each
+    point, then see what the effect of raising or lowering it will be.
+    
+    Might actually be easier to plot s vs S and then vary B - see what happens.
+    But do the phase spaces first because it's cool, and then maybe we can 
+    un-normalize some of those variables as well.
+    
+    Note: Currently not changing density or cold composition along field line,
+    could do!
+    '''
+    # Main inputs
+    f        = 3.0                         # Hz
+    dwdt     = 0*2 * np.pi * (1.2/48)      # Frequency sweep rate (rad/s2) 1.2Hz in 48sec
+    Bw       = 2.5e-9                      # Initial EMIC (triggering?) amplitude
+    vth_perp = 8e5                         #  m/s
+    #vth_para = 6e5                        # m/s
+    #nh       = 7.2e6                      # /m3
+    #Q_value  = 0.5                        # Depth of proton hole
+    
+    w    = 2*np.pi*f                   # rad/s
+    omw  = qp * Bw / mp                # 'Gyrofrequency' effect of wave field
+    
+    # Field parameters
     s, Bs, mlat = new_trace_fieldline(L_shell)
     B_grad      = get_B_gradient(Bs, s)
     Om_grad     = qp / mp * B_grad
     
-    Vg, Vp, Vr = get_velocities(w, k, Species, PP)
+    Vr_array    = np.zeros(s.shape[0])
+    wtr_array   = np.zeros(s.shape[0])
+    Vtr_array   = np.zeros(s.shape[0])
+    k_array     = np.zeros(s.shape[0])
+    S_ratio     = np.zeros(s.shape[0])
     
-    # Define s-values
-    s0 = vth_perp / Vp
-    s1 = (1.0 - Vr/Vg) ** 2
-    s2 = (vth_perp**2/(2*Vp**2) + Vr**2 / (Vp*Vg) - Vr**2/(2*Vp**2))*(w / pcyc) - Vr/Vp
-    
-    S_ratio = 1 / (s0 * w * omw) * (s1*dwdt + Vp*s2*Om_grad)
-    return mlat, S_ratio
+    # Calculate inhomogeneity ratio at each point along field
+    for ii in range(s.shape[0]):
+        Species, PP = define_omura2010_parameters(Bs[ii])
+        
+        # Wave parameters
+        k_array[ii]   = get_k_cold(w, Species)            # /m ??
+        pcyc          = PP['pcyc_rad']                    # Proton cyclotron frequency
+        wtr_array[ii] = np.sqrt(k_array[ii]*vth_perp*omw) # Trapping frequency
+        Vtr_array[ii] = 2 * wtr_array[ii] / k_array[ii]   # Trapping velocity
+        
+        Vg, Vp, Vr_array[ii] = get_velocities(w, k_array[ii], Species, PP)
+        
+        # Define s-values
+        s0 = vth_perp / Vp
+        s1 = (1.0 - Vr_array[ii]/Vg) ** 2
+        s2 = (vth_perp**2/(2*Vp**2) + Vr_array[ii]**2 / (Vp*Vg) - Vr_array[ii]**2/(2*Vp**2))*(w / pcyc) - Vr_array[ii]/Vp
+        
+        S_ratio[ii] = 1 / (s0 * w * omw) * (s1*dwdt + Vp*s2*Om_grad[ii])
+        
+    return s, mlat, S_ratio, Vr_array, wtr_array, Vtr_array, k_array
 
 
-if __name__ == '__main__':
-# =============================================================================
-#     S, BS, MLAT = new_trace_fieldline(4.27)
-#     GRAD        = get_B_gradient(BS, S)
-#     
-#     fig, ax = plt.subplots()
-#     ax.plot(S/RE, BS, c='b')
-#     ax.axvline(0)
-#     
-#     fig, ax = plt.subplots()
-#     ax.plot(S/RE, GRAD)
-# =============================================================================
+if __name__ == '__main__':    
+    main_folder = 'F://NONLINEAR//'
     
-    s, S_ratio = plot_SI_phase_space()
-    plt.plot(s, S_ratio)
-    plt.ylim(-10, 10)
+    L = 4.27
+    S, MLAT, S_RAT, VR, WTR, VTR, K = get_S_dipole(L)
+    
+    if True:
+        plt.figure()
+        plt.plot(S/1e3, S_RAT)
+        plt.ylabel('S ratio')
+        plt.xlabel('s (km)')
+        plt.ylim(-1, 1)
+        
+        plt.figure()
+        plt.plot(MLAT, S_RAT)
+        plt.ylabel('S ratio')
+        plt.xlabel('MLAT')
+        plt.ylim(-1, 1)
 
-# =============================================================================
-#     
-#     
-#     S      = 0.0
-#     zeta   = np.linspace(0, 2*np.pi, 250)
-#     theta  = np.linspace(-2, 2, 1000) 
-#     ze, th = np.meshgrid(zeta, theta)
-#     
-#     # Calculate curves of constancy
-#     const = np.zeros(th.shape)
-#     for ii in range(th.shape[0]):
-#         for jj in range(th.shape[1]):
-#             const[ii, jj] = th[ii, jj]**2 + 0.5 * (np.cos(ze[ii, jj]) - S * ze[ii, jj])
-#     
-#     # Plot curves
-#     plt.ioff()
-#     fig, ax = plt.subplots(figsize=(16, 10))
-#     ax.contour(ze, th, const, levels=75, colors='k', linestyles='-', alpha=0.5)
-# =============================================================================
+    if False:
+        # Plot phase space for each position along the field (only for S < 1.5)
+        run_folder = 'standard_dipole//'
+        save_dir   = main_folder + run_folder
+        if os.path.exists(save_dir) == False:
+            os.makedirs(save_dir)
+        
+        # Assume there's +/- S on either side of the equator
+        S_lim  = 1.5
+        st_idx = np.where(np.abs(S_lim - S_RAT) == np.abs(S_lim - S_RAT).min())[0][0]
+        en_idx = np.where(np.abs(S_lim + S_RAT) == np.abs(S_lim + S_RAT).min())[0][0]
+
+        # Don't un-normalize the const calculation yet, probably won't change anything
+        skip = 5;   counter = 0
+        for mm in range(st_idx, en_idx):
+            if counter%skip == 0:
+                print('Plotting {} of {}'.format(mm - st_idx, en_idx - st_idx))
+                zeta   = np.linspace(0, 2*np.pi, 250)
+                theta  = np.linspace(-2, 2, 500) 
+                ze, th = np.meshgrid(zeta, theta)
+                
+                # Calculate curves of constancy (njit this?)
+                const = np.zeros(th.shape)
+                for ii in range(th.shape[0]):
+                    for jj in range(th.shape[1]):
+                        const[ii, jj] = th[ii, jj]**2 + 0.5 * (np.cos(ze[ii, jj]) - S_RAT[mm] * ze[ii, jj])
+                        
+                # Work out const of separatrix
+                ze0         =   np.pi + np.arcsin(np.abs(S_RAT[mm]))
+                ze1         = 2*np.pi - np.arcsin(np.abs(S_RAT[mm]))    
+                separatrix  = 0.5*(np.cos(ze1) - S_RAT[mm]*ze1)
+                
+                # Plot curves
+                plt.ioff()
+                fig, ax = plt.subplots(figsize=(16, 10))
+                ax.contour(ze, th, const, levels=75, colors='k', linestyles='-', alpha=0.5)
+                ax.contour(ze, th, const, levels=[separatrix], colors='k', linestyles='-')
+                
+                ax.contourf(ze, th, const, levels=np.array([separatrix, const.max()]), colors='grey')
+                ax.axvspan(ze1, 2*np.pi, color='grey')
+                
+                ax.set_xlabel('$\zeta$', fontsize=20)
+                ax.set_ylabel('$\\frac{\\theta}{2 \omega_{tr}}$', rotation=0, labelpad=20, fontsize=20)
+                ax.set_title('Dipole Resonant Proton Phase Space :: S = %.2f :: L = %.2f :: MLAT = %.2f$^{\circ}$ :: s = %.0f km'
+                             % (S_RAT[mm], L, MLAT[mm], S[mm]/1e3), fontsize=14)
+                
+                fig.savefig(save_dir + 'phase_space_{:06}.png'.format(mm))
+                plt.close('all')
+            counter += 1
