b1149b69c9b0c79301dc16b5568de2e31484c568
Yoshi2112 <joshua.s.williams@uon.edu.au>
2021-02-21 21:54:57 +1100

    Put CAM_CL batch runs on their own numbers

--- simulation_codes/run_inputs/batch_runs/multispecies_SI_test/run2_CAM_CL.sh
+++ simulation_codes/run_inputs/batch_runs/multispecies_SI_test/run2_CAM_CL.sh
@@ -8,5 +8,5 @@
 source /etc/profile.d/modules.sh
 module load numba/0.49.1-python.3.6
 cd $PBS_O_WORKDIR
-python /home/c3134027/hybrid/simulation_codes/CAM_CL_1D_ONESCRIPT/main_1D.py -r /batch_runs/multispecies_SI_test/run_params_PU1024.run  -p /batch_runs/multispecies_SI_test/plasma_params_H_O.plasma -n 2
+python /home/c3134027/hybrid/simulation_codes/CAM_CL_1D_ONESCRIPT/main_1D.py -r /batch_runs/multispecies_SI_test/run_params_PU1024.run -p /batch_runs/multispecies_SI_test/plasma_params_H_O.plasma -n 6
 exit 0
