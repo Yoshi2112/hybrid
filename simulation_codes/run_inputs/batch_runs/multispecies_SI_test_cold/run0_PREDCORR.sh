36d75725f7841268968f827012f5a5559cf45fb0
Yoshi2112 <joshua.s.williams@uon.edu.au>
2021-02-22 09:11:19 +1100

    Fix cold batch runs

--- simulation_codes/run_inputs/batch_runs/multispecies_SI_test_cold/run0_PREDCORR.sh
+++ simulation_codes/run_inputs/batch_runs/multispecies_SI_test_cold/run0_PREDCORR.sh
@@ -8,5 +8,5 @@
 source /etc/profile.d/modules.sh
 module load numba/0.49.1-python.3.6
 cd $PBS_O_WORKDIR
-python /home/c3134027/hybrid/simulation_codes/PREDCORR_1D_PARALLEL/main_1D.py -r /batch_runs/multispecies_SI_test/run_params_PU1024.run  -p /batch_runs/multispecies_SI_test/plasma_params_H.plasma -n 0
+python /home/c3134027/hybrid/simulation_codes/PREDCORR_1D_PARALLEL/main_1D.py -r /batch_runs/multispecies_SI_test_cold/run_params_PU1024.run  -p /batch_runs/multispecies_SI_test_cold/plasma_params_H.plasma -n 0
 exit 0
